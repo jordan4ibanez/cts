8dbd4f3a4d342d360994712d270bede5b486064c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-28 01:19:53 -0400

    Update init.ts

--- source/hud_manager/init.ts
+++ source/hud_manager/init.ts
@@ -7,27 +7,29 @@ namespace hudManager {
 	    player_huds.delete(player.get_player_name())
 	})
 
-	// // create instance of new hud
-	// hud_manager.add_hud = function(player,hud_name,def)
-	//     local name = player:get_player_name()
-	//     local local_hud = player:hud_add({
-	// 		hud_elem_type = def.hud_elem_type,
-	// 		position      = def.position,
-	// 		text          = def.text,
-	// 		number        = def.number,
-	// 		direction     = def.direction,
-	// 		size          = def.size,
-	// 		offset        = def.offset,
-	//     })
-	//     // create new 3d array here
-	//     // depends.txt is not needed
-	//     // with it here
-	//     if not player_huds[name] then
-	//         player_huds[name] = {}
-	//     end
+	// create instance of new hud
+	export function add_hud(player: ObjectRef,hud_name: string,def: HudDefinition) {
 
-	//     player_huds[name][hud_name] = local_hud
-	// end
+	    const name: string = player.get_player_name();
+
+	    const local_hud = player.hud_add({
+			hud_elem_type : def.hud_elem_type,
+			position      : def.position,
+			text          : def.text,
+			number        : def.number,
+			direction     : def.direction,
+			size          : def.size,
+			offset        : def.offset,
+	    })
+	    // Create new 3d array here.
+	    // depends.txt is not needed
+	    // with it here
+	    if not player_huds[name] then
+	        player_huds[name] = {}
+	    end
+
+	    player_huds[name][hud_name] = local_hud
+    }
 
 	// // delete instance of hud
 	// hud_manager.remove_hud = function(player,hud_name)
