58491676f4702f9d000927eeffebd4afd3b4d7f1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-28 01:35:02 -0400

    Update init.ts

--- source/hud_manager/init.ts
+++ source/hud_manager/init.ts
@@ -32,7 +32,9 @@ namespace hudManager {
 			data = player_huds.get(name);
 			// Now if it's still null, something went seriously wrong.
 			if (!data) {
-				throw new Error("How did this become null? It was just created. Error");
+				throw new Error(
+					"How did this become null? It was just created. Error"
+				);
 			}
 		}
 
