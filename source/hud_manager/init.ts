64307de0ffaaa806327749a52b09d59c8cbf2009
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-28 16:31:08 -0400

    Anything that uses this needs to depend on it

--- source/hud_manager/init.ts
+++ source/hud_manager/init.ts
@@ -32,14 +32,7 @@ namespace hudManager {
 		let data: Map<string, number> | undefined = player_huds.get(name);
 
 		if (!data) {
-			player_huds.set(name, new Map<string, number>());
-			data = player_huds.get(name);
-			// Now if it's still null, something went seriously wrong.
-			if (!data) {
-				throw new Error(
-					"How did this become null? It was just created. Error"
-				);
-			}
+			throw new Error("Player's hud entry was never created. Error");
 		}
 
 		data.set(hud_name, local_hud);
