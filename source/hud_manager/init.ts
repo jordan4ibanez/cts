5e43457ac62838764caf2b91419a29421fdba2c3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-28 01:28:24 -0400

    It's never too late to fix old mistakes

--- source/hud_manager/init.ts
+++ source/hud_manager/init.ts
@@ -2,6 +2,10 @@ namespace hudManager {
 	// The list of players hud lists (3d array).
 	const player_huds = new Map<string, Map<string, number>>();
 
+	core.register_on_joinplayer((player: ObjectRef) => {
+		player_huds.set(player.get_player_name(), new Map<string, number>());
+	});
+
 	// Terminate the player's list on leave.
 	core.register_on_leaveplayer((player: ObjectRef) => {
 		player_huds.delete(player.get_player_name());
@@ -24,11 +28,6 @@ namespace hudManager {
 			size: def.size,
 			offset: def.offset,
 		});
-
-		// Create new 3d array here.
-		if (!player_huds.has(name)) {
-			player_huds.set(name, new Map<string, number>());
-		}
 
 		const data: Map<string, number> | undefined = player_huds.get(name);
 
