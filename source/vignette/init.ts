68f5c0d43c08d178fd3a0757dde4fcd1569be62c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-29 00:04:35 -0400

    I dunno why this isn't working

--- source/vignette/init.ts
+++ source/vignette/init.ts
@@ -1,21 +1,24 @@
 namespace vignette {
 	core.register_on_joinplayer((player: ObjectRef) => {
-		for (let i = 0; i < 10; i++) {
-			print(i);
-			hudManager.add_hud(
-				player,
-				"vignette",
+		// for (let i = 0; i < 100; i++) {
+		// 	print(i);
+		print("hi hi hi ");
+		print(HudElementType.image);
+		hudManager.add_hud(
+			player,
+			"vignette",
 
-				{
-					hud_elem_type: HudElementType.image,
-					position: { x: 0.5, y: 0.5 },
-					scale: {
-						x: -100.5,
-						y: -100.5,
-					},
-					text: "vignette.png",
-				}
-			);
-		}
+			{
+				type: HudElementType.image,
+				position: { x: 0.5, y: 0.5 },
+				scale: {
+					x: -50.5,
+					y: -50.5,
+				},
+				z_index: -400,
+				text: "vignette.png",
+			}
+		);
+		// }
 	});
 }
