eb63ec3bf1a8c1545c724204839c0c18c0a2f913
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-29 17:20:19 -0400

    formatting disaster

--- source/flow_lib/init.ts
+++ source/flow_lib/init.ts
@@ -5,95 +5,92 @@
 // minetest,vector,math,pairs
 
 namespace flowLib {
+	// local tab
+	// local n
 
-// local tab
-// local n
+	interface FlowNode {
+		pos: Vec3;
+		node: NodeTable;
+	}
 
-    interface FlowNode {
-        pos: Vec3,
-        node: NodeTable
-    }
+	function get_nodes(pos: Vec3) {
+		const tab: FlowNode[] = [];
+		const n: number = 1;
 
- function get_nodes(pos: Vec3) {
-	const tab: FlowNode[] = []
-    const n: number = 1
+		for (let i = -1; i <= 1; i += 2) {
+			const data = [
+				vector.create3d({ x: pos.x + i, y: pos.y, z: pos.z }),
+				vector.create3d({ x: pos.x, y: pos.y, z: pos.z + i }),
+			];
 
-	for (let i = -1; i <= 1; i+= 2) {
-        const data = [
-			vector.create3d({x:pos.x+i, y:pos.y, z:pos.z}),
-			vector.create3d({x:pos.x, y:pos.y, z:pos.z+i})
-		];
-        
-		for (const p of data) {
-            tab.push({
-                pos: p,
-                node: core.get_node(p),
-            })
-			
-        }
-    }
+			for (const p of data) {
+				tab.push({
+					pos: p,
+					node: core.get_node(p),
+				});
+			}
+		}
 
-	return tab
-}
+		return tab;
+	}
 
+	// local data
+	// local param2
+	// local nd
+	// local par2
+	// local name
+	// local tmp
+	// local c_node
+	// local function get_flowing_dir(pos)
+	// 	c_node = minetest.get_node(pos).name
+	// 	if c_node ~= "main:waterflow" and c_node ~= "main:water" then
+	// 		return nil
+	// 	end
+	// 	data = get_nodes(pos)
+	// 	param2 = minetest.get_node(pos).param2
+	// 	if param2 > 7 then
+	// 		return nil
+	// 	end
+	// 	if c_node == "main:water" then
+	// 		for _,i in pairs(data) do
+	// 			nd = i[2]
+	// 			name = nd.name
+	// 			par2 = nd.param2
+	// 			if name == "main:waterflow" and par2 == 7 then
+	// 				return(vector.subtract(i[1],pos))
+	// 			end
+	// 		end
+	// 	end
+	// 	for _,i in pairs(data) do
+	// 		nd = i[2]
+	// 		name = nd.name
+	// 		par2 = nd.param2
+	// 		if name == "main:waterflow" and par2 < param2 then
+	// 			return(vector.subtract(i[1],pos))
+	// 		end
+	// 	end
+	// 	for _,i in pairs(data) do
+	// 		nd = i[2]
+	// 		name = nd.name
+	// 		par2 = nd.param2
+	// 		if name == "main:waterflow" and par2 >= 11 then
+	// 			return(vector.subtract(i[1],pos))
+	// 		end
+	// 	end
+	// 	for _,i in pairs(data) do
+	// 		nd = i[2]
+	// 		name = nd.name
+	// 		par2 = nd.param2
+	// 		tmp = minetest.registered_nodes[name]
+	// 		if tmp and not tmp.walkable and name ~= "main:waterflow" and name ~= "main:water" then
+	// 			return(vector.subtract(i[1],pos))
+	// 		end
+	// 	end
 
-// local data
-// local param2
-// local nd
-// local par2
-// local name
-// local tmp
-// local c_node
-// local function get_flowing_dir(pos)
-// 	c_node = minetest.get_node(pos).name
-// 	if c_node ~= "main:waterflow" and c_node ~= "main:water" then
-// 		return nil
-// 	end
-// 	data = get_nodes(pos)
-// 	param2 = minetest.get_node(pos).param2
-// 	if param2 > 7 then
-// 		return nil
-// 	end
-// 	if c_node == "main:water" then
-// 		for _,i in pairs(data) do
-// 			nd = i[2]
-// 			name = nd.name
-// 			par2 = nd.param2
-// 			if name == "main:waterflow" and par2 == 7 then
-// 				return(vector.subtract(i[1],pos))
-// 			end
-// 		end
-// 	end
-// 	for _,i in pairs(data) do
-// 		nd = i[2]
-// 		name = nd.name
-// 		par2 = nd.param2
-// 		if name == "main:waterflow" and par2 < param2 then
-// 			return(vector.subtract(i[1],pos))
-// 		end
-// 	end
-// 	for _,i in pairs(data) do
-// 		nd = i[2]
-// 		name = nd.name
-// 		par2 = nd.param2
-// 		if name == "main:waterflow" and par2 >= 11 then
-// 			return(vector.subtract(i[1],pos))
-// 		end
-// 	end
-// 	for _,i in pairs(data) do
-// 		nd = i[2]
-// 		name = nd.name
-// 		par2 = nd.param2
-// 		tmp = minetest.registered_nodes[name]
-// 		if tmp and not tmp.walkable and name ~= "main:waterflow" and name ~= "main:water" then
-// 			return(vector.subtract(i[1],pos))
-// 		end
-// 	end
+	// 	return nil
+	// end
 
-// 	return nil
-// end
-
-// function flow(pos)
-// 	return(get_flowing_dir(pos))
-// end
+	// function flow(pos)
+	// 	return(get_flowing_dir(pos))
+	// end
 }
