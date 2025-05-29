53b8bb3c03572afb065d2571d7900ac9de5bac04
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-29 17:35:50 -0400

    Update init.ts

--- source/flow_lib/init.ts
+++ source/flow_lib/init.ts
@@ -64,7 +64,11 @@ namespace flowLib {
 			for (const i of data) {
 				const par2: number | undefined = i.node.param2;
 
-				if (i.node.name == "main:waterflow" && par2 && par2 == 7) {
+				if (
+					i.node.name == "main:waterflow" &&
+					par2 != null &&
+					par2 == 7
+				) {
 					return vector.subtract(i.pos, pos);
 				}
 			}
@@ -73,7 +77,7 @@ namespace flowLib {
 		for (const i of data) {
 			const name: string = i.node.name;
 			const par2: number | undefined = i.node.param2;
-			if (name == "main:waterflow" && par2 && par2 < param2) {
+			if (name == "main:waterflow" && par2 != null && par2 < param2) {
 				return vector.subtract(i.pos, pos);
 			}
 		}
