7b206c33162f2888a6f20347e019e2bed156e5fb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-29 00:39:05 -0400

    Update init.ts

--- source/vignette/init.ts
+++ source/vignette/init.ts
@@ -1,19 +1,14 @@
 namespace vignette {
 	core.register_on_joinplayer((player: ObjectRef) => {
-		hudManager.add_hud(
-			player,
-			"vignette",
-
-			{
-				type: HudElementType.image,
-				position: { x: 0.5, y: 0.5 },
-				scale: {
-					x: -100.5,
-					y: -100.5,
-				},
-				z_index: -400,
-				text: "vignette.png",
-			}
-		);
+		hudManager.add_hud(player, "vignette", {
+			type: HudElementType.image,
+			position: { x: 0.5, y: 0.5 },
+			scale: {
+				x: -100.5,
+				y: -100.5,
+			},
+			z_index: -400,
+			text: "vignette.png",
+		});
 	});
 }
