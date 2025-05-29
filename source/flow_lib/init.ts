b6c58b692d853c507767c88db3503b2c12b9d456
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-29 17:35:41 -0400

    Update init.ts

--- source/flow_lib/init.ts
+++ source/flow_lib/init.ts
@@ -70,14 +70,13 @@ namespace flowLib {
 			}
 		}
 
-		for _,i in pairs(data) do
-			nd = i[2]
-			name = nd.name
-			par2 = nd.param2
-			if name == "main:waterflow" and par2 < param2 then
-				return(vector.subtract(i[1],pos))
-			end
-		end
+		for (const i of data) {
+			const name: string = i.node.name;
+			const par2: number | undefined = i.node.param2;
+			if (name == "main:waterflow" && par2 && par2 < param2) {
+				return vector.subtract(i.pos, pos);
+			}
+		}
 
 		// for _,i in pairs(data) do
 		// 	nd = i[2]
