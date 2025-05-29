9d6612ad44aed9ae9fc60147bc85d18dd8770e34
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-28 23:09:16 -0400

    Update init.ts

--- source/walls/init.ts
+++ source/walls/init.ts
@@ -234,9 +234,9 @@ namespace walls {
 	// Set up wall.
 	const def2: NodeDefinition = table.copy(def as LuaTable) as NodeDefinition;
 	const newname: string = "walls:window";
-	def2.description = "Window"
-	def2.mod_origin = "walls"
-	def2.drop = ""
+	def2.description = "Window";
+	def2.mod_origin = "walls";
+	def2.drop = "";
 	// def2.paramtype = "light"
 	// def2.drawtype = "nodebox"
 	// def2.on_dig = nil
