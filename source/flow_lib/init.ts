7ab1e80814653b1c12e40b9519a173d7fc897ca4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-29 17:42:27 -0400

    Update init.ts

--- source/flow_lib/init.ts
+++ source/flow_lib/init.ts
@@ -1,8 +1,4 @@
 // this is from https://github.com/HybridDog/builtin_item/blob/e6dfd9dce86503b3cbd1474257eca5f6f6ca71c2/init.lua#L50
-// local
-// minetest,vector,math,pairs
-// =
-// minetest,vector,math,pairs
 
 namespace flowLib {
 	interface FlowNode {
