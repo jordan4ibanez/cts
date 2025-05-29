84dd0fbe222b115393cd3b2df5a1f83a69c3447a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-28 21:03:40 -0400

    Update init.ts

--- source/walls/init.ts
+++ source/walls/init.ts
@@ -2,57 +2,57 @@ namespace walls {
 local fence_collision_extra = 4/8
 
 //create fences for all solid nodes
-for name,def in pairs(core.registered_nodes) do
-	if def.drawtype == "normal" and string.match(name, "main:") then
+for (const [name,def] of pairs(core.registered_nodes)) {
+	// if def.drawtype == "normal" and string.match(name, "main:") then
 	
-		//set up fence
-		local def2 = table.copy(def)
-		local newname = "walls:"..string.gsub(name, "main:", "").."_fence"
-		def2.mod_origin = "walls"
-		def2.name = newname
-		def2.description = def.description.." Fence"
-		def2.drop = newname
-		def2.paramtype = "light"
-		def2.drawtype = "nodebox"
-		def2.on_dig = nil
-		def2.node_box = {
-			type = "connected",
-			fixed = {-1/8, -1/2, -1/8, 1/8, 1/2, 1/8},
-			// connect_top =
-			// connect_bottom =
-			connect_front = {{-1/16,  3/16, -1/2,   1/16,  5/16, -1/8 },
-				         {-1/16, -5/16, -1/2,   1/16, -3/16, -1/8 }},
-			connect_left =  {{-1/2,   3/16, -1/16, -1/8,   5/16,  1/16},
-				         {-1/2,  -5/16, -1/16, -1/8,  -3/16,  1/16}},
-			connect_back =  {{-1/16,  3/16,  1/8,   1/16,  5/16,  1/2 },
-				         {-1/16, -5/16,  1/8,   1/16, -3/16,  1/2 }},
-			connect_right = {{ 1/8,   3/16, -1/16,  1/2,   5/16,  1/16},
-				         { 1/8,  -5/16, -1/16,  1/2,  -3/16,  1/16}}
-		}
-		def2.collision_box = {
-			type = "connected",
-			fixed = {-1/8, -1/2, -1/8, 1/8, 1/2 + fence_collision_extra, 1/8},
-			// connect_top =
-			// connect_bottom =
-			connect_front = {-1/8, -1/2, -1/2,  1/8, 1/2 + fence_collision_extra, -1/8},
-			connect_left =  {-1/2, -1/2, -1/8, -1/8, 1/2 + fence_collision_extra,  1/8},
-			connect_back =  {-1/8, -1/2,  1/8,  1/8, 1/2 + fence_collision_extra,  1/2},
-			connect_right = { 1/8, -1/2, -1/8,  1/2, 1/2 + fence_collision_extra,  1/8}
-		}
-		def2.connects_to = {"group:fence", "group:wood", "group:tree", "group:wall", "group:stone", "group:sand"}
-		def2.sunlight_propagates = true
-		core.register_node(newname,def2)
+	// 	//set up fence
+	// 	local def2 = table.copy(def)
+	// 	local newname = "walls:"..string.gsub(name, "main:", "").."_fence"
+	// 	def2.mod_origin = "walls"
+	// 	def2.name = newname
+	// 	def2.description = def.description.." Fence"
+	// 	def2.drop = newname
+	// 	def2.paramtype = "light"
+	// 	def2.drawtype = "nodebox"
+	// 	def2.on_dig = nil
+	// 	def2.node_box = {
+	// 		type = "connected",
+	// 		fixed = {-1/8, -1/2, -1/8, 1/8, 1/2, 1/8},
+	// 		// connect_top =
+	// 		// connect_bottom =
+	// 		connect_front = {{-1/16,  3/16, -1/2,   1/16,  5/16, -1/8 },
+	// 			         {-1/16, -5/16, -1/2,   1/16, -3/16, -1/8 }},
+	// 		connect_left =  {{-1/2,   3/16, -1/16, -1/8,   5/16,  1/16},
+	// 			         {-1/2,  -5/16, -1/16, -1/8,  -3/16,  1/16}},
+	// 		connect_back =  {{-1/16,  3/16,  1/8,   1/16,  5/16,  1/2 },
+	// 			         {-1/16, -5/16,  1/8,   1/16, -3/16,  1/2 }},
+	// 		connect_right = {{ 1/8,   3/16, -1/16,  1/2,   5/16,  1/16},
+	// 			         { 1/8,  -5/16, -1/16,  1/2,  -3/16,  1/16}}
+	// 	}
+	// 	def2.collision_box = {
+	// 		type = "connected",
+	// 		fixed = {-1/8, -1/2, -1/8, 1/8, 1/2 + fence_collision_extra, 1/8},
+	// 		// connect_top =
+	// 		// connect_bottom =
+	// 		connect_front = {-1/8, -1/2, -1/2,  1/8, 1/2 + fence_collision_extra, -1/8},
+	// 		connect_left =  {-1/2, -1/2, -1/8, -1/8, 1/2 + fence_collision_extra,  1/8},
+	// 		connect_back =  {-1/8, -1/2,  1/8,  1/8, 1/2 + fence_collision_extra,  1/2},
+	// 		connect_right = { 1/8, -1/2, -1/8,  1/2, 1/2 + fence_collision_extra,  1/8}
+	// 	}
+	// 	def2.connects_to = {"group:fence", "group:wood", "group:tree", "group:wall", "group:stone", "group:sand"}
+	// 	def2.sunlight_propagates = true
+	// 	core.register_node(newname,def2)
 		
-		core.register_craft({
-			output = newname .. " 16",
-			recipe = {
-				{ name, 'main:stick', name },
-				{ name, 'main:stick', name },
-			}
-		})
+	// 	core.register_craft({
+	// 		output = newname .. " 16",
+	// 		recipe = {
+	// 			{ name, 'main:stick', name },
+	// 			{ name, 'main:stick', name },
+	// 		}
+	// 	})
 		
-	end
-end
+	// end
+    }
 
 // //create wall posts
 // for name,def in pairs(core.registered_nodes) do
