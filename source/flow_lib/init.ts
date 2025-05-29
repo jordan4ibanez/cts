ad70594accae5a96e041b2d0130904a64b42600c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-29 17:04:20 -0400

    Update init.ts

--- source/flow_lib/init.ts
+++ source/flow_lib/init.ts
@@ -23,63 +23,63 @@ local function get_nodes(pos)
 end
 
 
-local data
-local param2
-local nd
-local par2
-local name
-local tmp
-local c_node
-local function get_flowing_dir(pos)
-	c_node = minetest.get_node(pos).name
-	if c_node ~= "main:waterflow" and c_node ~= "main:water" then
-		return nil
-	end
-	data = get_nodes(pos)
-	param2 = minetest.get_node(pos).param2
-	if param2 > 7 then
-		return nil
-	end
-	if c_node == "main:water" then
-		for _,i in pairs(data) do
-			nd = i[2]
-			name = nd.name
-			par2 = nd.param2
-			if name == "main:waterflow" and par2 == 7 then
-				return(vector.subtract(i[1],pos))
-			end
-		end
-	end
-	for _,i in pairs(data) do
-		nd = i[2]
-		name = nd.name
-		par2 = nd.param2
-		if name == "main:waterflow" and par2 < param2 then
-			return(vector.subtract(i[1],pos))
-		end
-	end
-	for _,i in pairs(data) do
-		nd = i[2]
-		name = nd.name
-		par2 = nd.param2
-		if name == "main:waterflow" and par2 >= 11 then
-			return(vector.subtract(i[1],pos))
-		end
-	end
-	for _,i in pairs(data) do
-		nd = i[2]
-		name = nd.name
-		par2 = nd.param2
-		tmp = minetest.registered_nodes[name]
-		if tmp and not tmp.walkable and name ~= "main:waterflow" and name ~= "main:water" then
-			return(vector.subtract(i[1],pos))
-		end
-	end
+// local data
+// local param2
+// local nd
+// local par2
+// local name
+// local tmp
+// local c_node
+// local function get_flowing_dir(pos)
+// 	c_node = minetest.get_node(pos).name
+// 	if c_node ~= "main:waterflow" and c_node ~= "main:water" then
+// 		return nil
+// 	end
+// 	data = get_nodes(pos)
+// 	param2 = minetest.get_node(pos).param2
+// 	if param2 > 7 then
+// 		return nil
+// 	end
+// 	if c_node == "main:water" then
+// 		for _,i in pairs(data) do
+// 			nd = i[2]
+// 			name = nd.name
+// 			par2 = nd.param2
+// 			if name == "main:waterflow" and par2 == 7 then
+// 				return(vector.subtract(i[1],pos))
+// 			end
+// 		end
+// 	end
+// 	for _,i in pairs(data) do
+// 		nd = i[2]
+// 		name = nd.name
+// 		par2 = nd.param2
+// 		if name == "main:waterflow" and par2 < param2 then
+// 			return(vector.subtract(i[1],pos))
+// 		end
+// 	end
+// 	for _,i in pairs(data) do
+// 		nd = i[2]
+// 		name = nd.name
+// 		par2 = nd.param2
+// 		if name == "main:waterflow" and par2 >= 11 then
+// 			return(vector.subtract(i[1],pos))
+// 		end
+// 	end
+// 	for _,i in pairs(data) do
+// 		nd = i[2]
+// 		name = nd.name
+// 		par2 = nd.param2
+// 		tmp = minetest.registered_nodes[name]
+// 		if tmp and not tmp.walkable and name ~= "main:waterflow" and name ~= "main:water" then
+// 			return(vector.subtract(i[1],pos))
+// 		end
+// 	end
 
-	return nil
-end
+// 	return nil
+// end
 
-function flow(pos)
-	return(get_flowing_dir(pos))
-end
+// function flow(pos)
+// 	return(get_flowing_dir(pos))
+// end
 }
