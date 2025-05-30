5c9921e24929010dca255c70ef8371267af6de24
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-30 01:22:45 -0400

    So this causes less problems

--- minetest-api.d.ts
+++ minetest-api.d.ts
@@ -1591,7 +1591,7 @@ declare global {
 	}
 
 	/** @noSelf **/ type Dictionary<K extends string | number | symbol, V> = {
-		[id in K]: V | null;
+		[id in K]?: V;
 	};
 
 	/** @noSelf **/ interface PlayerControlObject
