b10c9231803c9c9b6d438fbe5df35fb3b7a8bb52
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-30 18:55:59 -0400

    Update chest.ts

--- source/furnace_chest/chest.ts
+++ source/furnace_chest/chest.ts
@@ -118,149 +118,149 @@ namespace furnace_chest {
 	}
 
 	export function register_chest(name: string, d: NodeDefinition) {
-		const def: NodeDefinition = table.copy(d as LuaTable) as NodeDefinition
-	// 	def.drawtype = "mesh"
-	// 	def.visual = "mesh"
-	// 	def.paramtype = "light"
-	// 	def.paramtype2 = "facedir"
-	// 	def.legacy_facedir_simple = true
-	// 	def.is_ground_content = false
-	// 	if def.protected then
-	// 		def.on_construct = function(pos)
-	// 			local meta = core.get_meta(pos)
-	// 			//meta:set_string("infotext", S("Locked Chest"))
-	// 			meta:set_string("owner", "")
-	// 			local inv = meta:get_inventory()
-	// 			inv:set_size("main", 9*4)
-	// 		end
-	// 		def.after_place_node = function(pos, placer)
-	// 			local meta = core.get_meta(pos)
-	// 			meta:set_string("owner", placer:get_player_name() or "")
-	// 			//meta:set_string("infotext", S("Locked Chest (owned by @1)", meta:get_string("owner")))
-	// 		end
-	// 		def.allow_metadata_inventory_move = function(pos, from_list, from_index,
-	// 				to_list, to_index, count, player)
-	// 			if not default.can_interact_with_node(player, pos) then
-	// 				return 0
-	// 			end
-	// 			return count
-	// 		end
-	// 		def.allow_metadata_inventory_put = function(pos, listname, index, stack, player)
-	// 			if not default.can_interact_with_node(player, pos) then
-	// 				return 0
-	// 			end
-	// 			return stack:get_count()
-	// 		end
-	// 		def.allow_metadata_inventory_take = function(pos, listname, index, stack, player)
-	// 			if not default.can_interact_with_node(player, pos) then
-	// 				return 0
-	// 			end
-	// 			return stack:get_count()
-	// 		end
-	// 		def.on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
-	// 			if not default.can_interact_with_node(clicker, pos) then
-	// 				return itemstack
-	// 			end
-	// 			core.sound_play(def.sound_open, {gain = 0.3,
-	// 					pos = pos, max_hear_distance = 10}, true)
-	// 				core.swap_node(pos,
-	// 						{ name = "utility:" .. name .. "_open",
-	// 						param2 = node.param2 })
-	// 			 core.show_formspec(clicker:get_player_name(),"utility:chest", chest.get_chest_formspec(pos))
-	// 			chest.open_chests[clicker:get_player_name()] = { pos = pos,
-	// 					sound = def.sound_close, swap = name }
-	// 		end
-	// 		def.on_blast = function() end
-	// 		def.on_key_use = function(pos, player)
-	// 			local secret = core.get_meta(pos):get_string("key_lock_secret")
-	// 			local itemstack = player:get_wielded_item()
-	// 			local key_meta = itemstack:get_meta()
-	// 			if itemstack:get_metadata() == "" then
-	// 				return
-	// 			end
-	// 			if key_meta:get_string("secret") == "" then
-	// 				key_meta:set_string("secret", core.parse_json(itemstack:get_metadata()).secret)
-	// 				itemstack:set_metadata("")
-	// 			end
-	// 			if secret ~= key_meta:get_string("secret") then
-	// 				return
-	// 			end
-	// 			core.show_formspec(
-	// 				player:get_player_name(),
-	// 				"utility:chest_locked",
-	// 				chest.get_chest_formspec(pos)
-	// 			)
-	// 		end
-	// 		def.on_skeleton_key_use = function(pos, player, newsecret)
-	// 			local meta = core.get_meta(pos)
-	// 			local owner = meta:get_string("owner")
-	// 			local pn = player:get_player_name()
-	// 			// verify placer is owner of lockable chest
-	// 			if owner ~= pn then
-	// 				core.record_protection_violation(pos, pn)
-	// 				//core.chat_send_player(pn, S("You do not own this chest."))
-	// 				return nil
-	// 			end
-	// 			local secret = meta:get_string("key_lock_secret")
-	// 			if secret == "" then
-	// 				secret = newsecret
-	// 				meta:set_string("key_lock_secret", secret)
-	// 			end
-	// 			//return secret, S("a locked chest"), owner
-	// 		end
-	// 	else
-	// 		def.on_construct = function(pos)
-	// 			local meta = core.get_meta(pos)
-	// 			//meta:set_string("infotext", S("Chest"))
-	// 			local inv = meta:get_inventory()
-	// 			inv:set_size("main", 9*4)
-	// 		end
-	// 		def.on_rightclick = function(pos, node, clicker)
-	// 			if core.get_node(pos).name ~= "utility:chest" and  core.get_node(pos).name ~= "utility:chest_open" then
-	// 				return
-	// 			end
-	// 			core.sound_play(def.sound_open, {gain = 0.3, pos = pos, max_hear_distance = 10}, true)
-	// 			core.swap_node(pos, {name = "utility:" .. name .. "_open", param2 = node.param2 })
-	// 			core.show_formspec(clicker:get_player_name(),"utility:chest", chest.get_chest_formspec(pos))
-	// 			chest.open_chests[clicker:get_player_name()] = { pos = pos,sound = def.sound_close, swap = name }
-	// 			//redstone.collect_info(pos)
-	// 		end
-	// 		def.on_blast = function(pos)
-	// 			local drops = {}
-	// 			default.get_inventory_drops(pos, "main", drops)
-	// 			drops[#drops+1] = "utility:" .. name
-	// 			core.remove_node(pos)
-	// 			return drops
-	// 		end
-	// 	end
-	// 	def.on_destruct = function(pos)
-	// 		destroy_chest(pos)
-	// 	end
-	// 	local def_opened = table.copy(def)
-	// 	local def_closed = table.copy(def)
-	// 	def_opened.mesh = "chest_open.obj"
-	// 	for i = 1, #def_opened.tiles do
-	// 		if type(def_opened.tiles[i]) == "string" then
-	// 			def_opened.tiles[i] = {name = def_opened.tiles[i], backface_culling = true}
-	// 		elseif def_opened.tiles[i].backface_culling == nil then
-	// 			def_opened.tiles[i].backface_culling = true
-	// 		end
-	// 	end
-	// 	def_opened.drop = "utility:" .. name
-	// 	def_opened.groups.not_in_creative_inventory = 1
-	// 	def_opened.selection_box = {
-	// 		type = "fixed",
-	// 		fixed = { -1/2, -1/2, -1/2, 1/2, 3/16, 1/2 },
-	// 	}
-	// 	def_opened.on_blast = function() end
-	// 	def_closed.mesh = nil
-	// 	def_closed.drawtype = nil
-	// 	def_closed.tiles[6] = def.tiles[5] // swap textures around for "normal"
-	// 	def_closed.tiles[5] = def.tiles[3] // drawtype to make them match the mesh
-	// 	def_closed.tiles[3] = def.tiles[3].."^[transformFX"
-	// 	core.register_node("utility:" .. name, def_closed)
-	// 	core.register_node("utility:" .. name .. "_open", def_opened)
-    }
+		const def: NodeDefinition = table.copy(d as LuaTable) as NodeDefinition;
+		// 	def.drawtype = "mesh"
+		// 	def.visual = "mesh"
+		// 	def.paramtype = "light"
+		// 	def.paramtype2 = "facedir"
+		// 	def.legacy_facedir_simple = true
+		// 	def.is_ground_content = false
+		// 	if def.protected then
+		// 		def.on_construct = function(pos)
+		// 			local meta = core.get_meta(pos)
+		// 			//meta:set_string("infotext", S("Locked Chest"))
+		// 			meta:set_string("owner", "")
+		// 			local inv = meta:get_inventory()
+		// 			inv:set_size("main", 9*4)
+		// 		end
+		// 		def.after_place_node = function(pos, placer)
+		// 			local meta = core.get_meta(pos)
+		// 			meta:set_string("owner", placer:get_player_name() or "")
+		// 			//meta:set_string("infotext", S("Locked Chest (owned by @1)", meta:get_string("owner")))
+		// 		end
+		// 		def.allow_metadata_inventory_move = function(pos, from_list, from_index,
+		// 				to_list, to_index, count, player)
+		// 			if not default.can_interact_with_node(player, pos) then
+		// 				return 0
+		// 			end
+		// 			return count
+		// 		end
+		// 		def.allow_metadata_inventory_put = function(pos, listname, index, stack, player)
+		// 			if not default.can_interact_with_node(player, pos) then
+		// 				return 0
+		// 			end
+		// 			return stack:get_count()
+		// 		end
+		// 		def.allow_metadata_inventory_take = function(pos, listname, index, stack, player)
+		// 			if not default.can_interact_with_node(player, pos) then
+		// 				return 0
+		// 			end
+		// 			return stack:get_count()
+		// 		end
+		// 		def.on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+		// 			if not default.can_interact_with_node(clicker, pos) then
+		// 				return itemstack
+		// 			end
+		// 			core.sound_play(def.sound_open, {gain = 0.3,
+		// 					pos = pos, max_hear_distance = 10}, true)
+		// 				core.swap_node(pos,
+		// 						{ name = "utility:" .. name .. "_open",
+		// 						param2 = node.param2 })
+		// 			 core.show_formspec(clicker:get_player_name(),"utility:chest", chest.get_chest_formspec(pos))
+		// 			chest.open_chests[clicker:get_player_name()] = { pos = pos,
+		// 					sound = def.sound_close, swap = name }
+		// 		end
+		// 		def.on_blast = function() end
+		// 		def.on_key_use = function(pos, player)
+		// 			local secret = core.get_meta(pos):get_string("key_lock_secret")
+		// 			local itemstack = player:get_wielded_item()
+		// 			local key_meta = itemstack:get_meta()
+		// 			if itemstack:get_metadata() == "" then
+		// 				return
+		// 			end
+		// 			if key_meta:get_string("secret") == "" then
+		// 				key_meta:set_string("secret", core.parse_json(itemstack:get_metadata()).secret)
+		// 				itemstack:set_metadata("")
+		// 			end
+		// 			if secret ~= key_meta:get_string("secret") then
+		// 				return
+		// 			end
+		// 			core.show_formspec(
+		// 				player:get_player_name(),
+		// 				"utility:chest_locked",
+		// 				chest.get_chest_formspec(pos)
+		// 			)
+		// 		end
+		// 		def.on_skeleton_key_use = function(pos, player, newsecret)
+		// 			local meta = core.get_meta(pos)
+		// 			local owner = meta:get_string("owner")
+		// 			local pn = player:get_player_name()
+		// 			// verify placer is owner of lockable chest
+		// 			if owner ~= pn then
+		// 				core.record_protection_violation(pos, pn)
+		// 				//core.chat_send_player(pn, S("You do not own this chest."))
+		// 				return nil
+		// 			end
+		// 			local secret = meta:get_string("key_lock_secret")
+		// 			if secret == "" then
+		// 				secret = newsecret
+		// 				meta:set_string("key_lock_secret", secret)
+		// 			end
+		// 			//return secret, S("a locked chest"), owner
+		// 		end
+		// 	else
+		// 		def.on_construct = function(pos)
+		// 			local meta = core.get_meta(pos)
+		// 			//meta:set_string("infotext", S("Chest"))
+		// 			local inv = meta:get_inventory()
+		// 			inv:set_size("main", 9*4)
+		// 		end
+		// 		def.on_rightclick = function(pos, node, clicker)
+		// 			if core.get_node(pos).name ~= "utility:chest" and  core.get_node(pos).name ~= "utility:chest_open" then
+		// 				return
+		// 			end
+		// 			core.sound_play(def.sound_open, {gain = 0.3, pos = pos, max_hear_distance = 10}, true)
+		// 			core.swap_node(pos, {name = "utility:" .. name .. "_open", param2 = node.param2 })
+		// 			core.show_formspec(clicker:get_player_name(),"utility:chest", chest.get_chest_formspec(pos))
+		// 			chest.open_chests[clicker:get_player_name()] = { pos = pos,sound = def.sound_close, swap = name }
+		// 			//redstone.collect_info(pos)
+		// 		end
+		// 		def.on_blast = function(pos)
+		// 			local drops = {}
+		// 			default.get_inventory_drops(pos, "main", drops)
+		// 			drops[#drops+1] = "utility:" .. name
+		// 			core.remove_node(pos)
+		// 			return drops
+		// 		end
+		// 	end
+		// 	def.on_destruct = function(pos)
+		// 		destroy_chest(pos)
+		// 	end
+		// 	local def_opened = table.copy(def)
+		// 	local def_closed = table.copy(def)
+		// 	def_opened.mesh = "chest_open.obj"
+		// 	for i = 1, #def_opened.tiles do
+		// 		if type(def_opened.tiles[i]) == "string" then
+		// 			def_opened.tiles[i] = {name = def_opened.tiles[i], backface_culling = true}
+		// 		elseif def_opened.tiles[i].backface_culling == nil then
+		// 			def_opened.tiles[i].backface_culling = true
+		// 		end
+		// 	end
+		// 	def_opened.drop = "utility:" .. name
+		// 	def_opened.groups.not_in_creative_inventory = 1
+		// 	def_opened.selection_box = {
+		// 		type = "fixed",
+		// 		fixed = { -1/2, -1/2, -1/2, 1/2, 3/16, 1/2 },
+		// 	}
+		// 	def_opened.on_blast = function() end
+		// 	def_closed.mesh = nil
+		// 	def_closed.drawtype = nil
+		// 	def_closed.tiles[6] = def.tiles[5] // swap textures around for "normal"
+		// 	def_closed.tiles[5] = def.tiles[3] // drawtype to make them match the mesh
+		// 	def_closed.tiles[3] = def.tiles[3].."^[transformFX"
+		// 	core.register_node("utility:" .. name, def_closed)
+		// 	core.register_node("utility:" .. name .. "_open", def_opened)
+	}
 
 	// chest.register_chest("chest", {
 	// 	description = "Chest",
