509226a1aea7c8782f58632a9922e76ef43f7a3d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-30 19:37:05 -0400

    Update chest.ts

--- source/furnace_chest/chest.ts
+++ source/furnace_chest/chest.ts
@@ -1,14 +1,14 @@
 namespace furnace_chest {
 	function can_interact_with_node(player: ObjectRef, pos: Vec3) {
-		// if player and player:is_player() then
-		// 	if minetest.check_player_privs(player, "protection_bypass") then
-		// 		return true
-		// 	end
-		// else
-		// 	return false
-		// end
+		if (player && player.is_player()) {
+			if core.check_player_privs(player, "protection_bypass") then
+				return true
+			end
+        }else{
+			return false
+        }
 
-		// local meta = minetest.get_meta(pos)
+		// local meta = core.get_meta(pos)
 		// local owner = meta:get_string("owner")
 
 		// if not owner or owner == "" or owner == player:get_player_name() then
@@ -17,16 +17,16 @@ namespace furnace_chest {
 
 		// // Is player wielding the right key?
 		// local item = player:get_wielded_item()
-		// if minetest.get_item_group(item:get_name(), "key") == 1 then
+		// if core.get_item_group(item:get_name(), "key") == 1 then
 		// 	local key_meta = item:get_meta()
 
 		// 	if key_meta:get_string("secret") == "" then
 		// 		local key_oldmeta = item:get_metadata()
-		// 		if key_oldmeta == "" or not minetest.parse_json(key_oldmeta) then
+		// 		if key_oldmeta == "" or not core.parse_json(key_oldmeta) then
 		// 			return false
 		// 		end
 
-		// 		key_meta:set_string("secret", minetest.parse_json(key_oldmeta).secret)
+		// 		key_meta:set_string("secret", core.parse_json(key_oldmeta).secret)
 		// 		item:set_metadata("")
 		// 	end
 
