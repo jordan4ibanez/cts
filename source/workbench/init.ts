e285093ad816da8579dd94c15f71da0f6b426c36
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-29 22:50:06 -0400

    Workbench

--- source/workbench/init.ts
+++ source/workbench/init.ts
@@ -1,17 +1,23 @@
 namespace workbench {
-    core.register_node("crafter_workbench:workbench", {
-        description : "Crafting Table",
-        tiles : ["crafting_workbench_top.png", "wood.png", "crafting_workbench_side.png",
-            "crafting_workbench_side.png", "crafting_workbench_front.png", "crafting_workbench_front.png"],
-        groups : {wood : 1, hard : 1, axe : 1, hand : 3,pathable : 1},
-        sounds : crafter.woodSound(),
-    })
+	core.register_node("crafter_workbench:workbench", {
+		description: "Workbench",
+		tiles: [
+			"crafting_workbench_top.png",
+			"wood.png",
+			"crafting_workbench_side.png",
+			"crafting_workbench_side.png",
+			"crafting_workbench_front.png",
+			"crafting_workbench_front.png",
+		],
+		groups: { wood: 1, hard: 1, axe: 1, hand: 3, pathable: 1 },
+		sounds: crafter.woodSound(),
+	});
 
-    core.register_craft({
-        output : "crafter_workbench:workbench",
-        recipe : [
-            ["main:wood","main:wood"],
-            ["main:wood","main:wood"]
-        ]
-    })
+	core.register_craft({
+		output: "crafter_workbench:workbench",
+		recipe: [
+			["main:wood", "main:wood"],
+			["main:wood", "main:wood"],
+		],
+	});
 }
