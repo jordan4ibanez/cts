2cb578400ebe6648c0e50966b3b79273f4c514d2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-30 01:30:50 -0400

    Update nodes.ts

--- source/crafter/nodes.ts
+++ source/crafter/nodes.ts
@@ -382,10 +382,10 @@ core.register_node("crafter:tree", {
 		}
 
 		const sneak: boolean = placer.get_player_control().sneak;
-		const noddef: NodeDefinition =
+		const noddef: NodeDefinition | undefined =
 			core.registered_nodes[core.get_node(pointed_thing.under).name];
 
-		if (!sneak && noddef.on_rightclick) {
+		if (!sneak && noddef && noddef.on_rightclick) {
 			core.item_place(itemstack, placer, pointed_thing);
 			return null;
 		}
