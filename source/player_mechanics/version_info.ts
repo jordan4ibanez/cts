f9c1f939fea3cb49835c479544d54d24edbad492
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-31 18:47:27 -0400

    Create version_info.ts

---
+++ source/player_mechanics/version_info.ts
@@ -0,0 +1,24 @@
+local minetest = minetest
+minetest.register_on_joinplayer(function(player)
+	--add in version info
+	player:hud_add({
+        hud_elem_type = "text",
+        position = {x=1, y=0},
+        name = "versionbg",
+        text = "Alpha 0.07",
+        number = 0x000000,
+        offset = {x = -98, y = 20},
+        size = { x=2, y=2 },
+        z_index = 0,
+    })                            
+    player:hud_add({
+        hud_elem_type = "text",
+        position = {x=1, y=0},
+        name = "versionfg",
+        text = "Alpha 0.07",
+        number = 0xFFFFFF,
+        offset = {x = -100, y = 18},
+        size = { x=2, y=2 },
+        z_index = 0,
+    }) 
+end)
