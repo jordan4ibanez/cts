30a079178414718ea4bbf749c08bbc0617f0bb92
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-31 02:39:46 -0400

    Update init.ts

--- source/too_many_items/init.ts
+++ source/too_many_items/init.ts
@@ -1,7 +1,7 @@
 namespace tooManyItems {
 	// local minetest,pairs = minetest,pairs
 
-    // todo: this needs to be able to scroll through all the recipes of an item!
+	// todo: this needs to be able to scroll through all the recipes of an item!
 
 	//? Note: This is being treated as a fortran module in this namespace.
 	class MasterInventory {
