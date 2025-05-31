f27b33cc3195edc0efda1adc6e220e2940625595
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-30 22:17:49 -0400

    Update init.ts

--- source/too_many_items/init.ts
+++ source/too_many_items/init.ts
@@ -164,33 +164,33 @@ namespace tooManyItems {
 			if (usable_table == null) {
 				throw new Error(`Recipe is null for [${item}]`);
 			}
-			// 			//shaped (regular)
-			// 			if recipe.width > 0 then
-			// 				for x = 1,3 do
-			// 					for y = 1,3 do
-			// 						item = get_if_group(usable_table[x][y])
-			// 						if item then
-			// 							output = output.."item_image_button["..base_x+y..","..base_y+x..";1,1;"..item..";"..item..";]"
-			// 						else
-			// 							output = output.."item_image_button["..base_x+y..","..base_y+x..";1,1;;;]"
-			// 						end
-			// 					end
-			// 				end
-			// 			//shapeless
-			// 			else
-			// 				local i = 1
-			// 				for x = 1,3 do
-			// 					for y = 1,3 do
-			// 						item = get_if_group(usable_table[i])
-			// 						if item then
-			// 							output = output.."item_image_button["..base_x+y..","..base_y+x..";1,1;"..item..";"..item..";]"
-			// 						else
-			// 							output = output.."item_image_button["..base_x+y..","..base_y+x..";1,1;;;]"
-			// 						end
-			// 						i = i + 1
-			// 					end
-			// 				end
-			// 			end
+			//shaped (regular)
+			if (recipe.width > 0) {
+				// 				for x = 1,3 do
+				// 					for y = 1,3 do
+				// 						item = get_if_group(usable_table[x][y])
+				// 						if item then
+				// 							output = output.."item_image_button["..base_x+y..","..base_y+x..";1,1;"..item..";"..item..";]"
+				// 						else
+				// 							output = output.."item_image_button["..base_x+y..","..base_y+x..";1,1;;;]"
+				// 						end
+				// 					end
+				// 				end
+				//shapeless
+			} else {
+				// 				local i = 1
+				// 				for x = 1,3 do
+				// 					for y = 1,3 do
+				// 						item = get_if_group(usable_table[i])
+				// 						if item then
+				// 							output = output.."item_image_button["..base_x+y..","..base_y+x..";1,1;"..item..";"..item..";]"
+				// 						else
+				// 							output = output.."item_image_button["..base_x+y..","..base_y+x..";1,1;;;]"
+				// 						end
+				// 						i = i + 1
+				// 					end
+				// 				end
+			}
 		} else if (recipe.method == CraftCheckType.cooking) {
 			// 		item = recipe.items[1]
 			// 		output = output.."item_image_button["..(base_x+2)..","..(base_y+1)..";1,1;"..item..";"..item..";]"
