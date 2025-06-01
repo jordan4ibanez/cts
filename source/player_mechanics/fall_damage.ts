47878828fc5982ea23a97fa85aa7706a8974aa0c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-01 02:42:07 -0400

    Update fall_damage.ts

--- source/player_mechanics/fall_damage.ts
+++ source/player_mechanics/fall_damage.ts
@@ -1,19 +1,11 @@
 namespace playerMechanics {
-	// local pos
-	// local name
-	// local saving_nodes
-	// local real_nodes
-	// local a_min
-	// local a_max
-	// local _
-
-	function cancel_fall_damage(player: ObjectRef) {
-		// 	name = player:get_player_name()
-		// 	if player:get_hp() <= 0 then
-		// 		return
-		// 	end
-		// 	-- used for finding a damage node from the center of the player
-		// 	-- rudementary collision detection
+	function cancel_fall_damage(player: ObjectRef): boolean {
+		const name: string = player.get_player_name();
+		if (player.get_hp() <= 0) {
+			return false;
+		}
+		// 	// used for finding a damage node from the center of the player
+		// 	// rudementary collision detection
 		// 	pos = player:get_pos()
 		// 	pos.y = pos.y
 		// 	a_min = vector.new(
@@ -26,18 +18,18 @@ namespace playerMechanics {
 		// 		pos.y+0.85,
 		// 		pos.z+0.25
 		// 	)
-		// 	_,saving_nodes = minetest.find_nodes_in_area( a_min,  a_max, {"group:disable_fall_damage"})
+		// 	_,saving_nodes = core.find_nodes_in_area( a_min,  a_max, {"group:disable_fall_damage"})
 		// 	real_nodes = {}
 		// 	for node_data,_ in pairs(saving_nodes) do
 		// 		if saving_nodes[node_data] > 0 then
 		// 			table.insert(real_nodes,node_data)
 		// 		end
 		// 	end
-		// 	-- find the highest damage node
+		// 	// find the highest damage node
 		// 	if table.getn(real_nodes) > 0 then
 		// 		return(true)
 		// 	end
-		// 	return(false)
+		return false;
 	}
 
 	// local function calc_fall_damage(player,hp_change)
@@ -49,20 +41,20 @@ namespace playerMechanics {
 	// 		local name = stack:get_name()
 	// 		if name ~= "" then
 	// 			local absorption = 0
-	// 			absorption = minetest.get_item_group(name,"armor_level")*2
-	// 			--print("absorbtion:",absorption)
-	// 			local wear_level = ((9-minetest.get_item_group(name,"armor_level"))*8)*(5-minetest.get_item_group(name,"armor_type"))*math.abs(hp_change)
+	// 			absorption = core.get_item_group(name,"armor_level")*2
+	// 			//print("absorbtion:",absorption)
+	// 			local wear_level = ((9-core.get_item_group(name,"armor_level"))*8)*(5-core.get_item_group(name,"armor_type"))*math.abs(hp_change)
 	// 			stack:add_wear(wear_level)
 	// 			inv:set_stack("armor_feet", 1, stack)
 	// 			local new_stack = inv:get_stack("armor_feet",1):get_name()
 	// 			if new_stack == "" then
-	// 				minetest.sound_play("armor_break",{to_player=player:get_player_name(),gain=1,pitch=math.random(80,100)/100})
+	// 				core.sound_play("armor_break",{to_player=player:get_player_name(),gain=1,pitch=math.random(80,100)/100})
 	// 				recalculate_armor(player)
 	// 				set_armor_gui(player)
-	// 				--do particles too
-	// 			elseif minetest.get_item_group(new_stack,"boots") > 0 then
+	// 				//do particles too
+	// 			elseif core.get_item_group(new_stack,"boots") > 0 then
 	// 				local pos = player:get_pos()
-	// 				minetest.add_particlespawner({
+	// 				core.add_particlespawner({
 	// 					amount = 30,
 	// 					time = 0.00001,
 	// 					minpos = {x=pos.x-0.5, y=pos.y+0.1, z=pos.z-0.5},
@@ -75,25 +67,25 @@ namespace playerMechanics {
 	// 					maxexptime = 1.5,
 	// 					minsize = 0,
 	// 					maxsize = 0,
-	// 					--attached = player,
+	// 					//attached = player,
 	// 					collisiondetection = true,
 	// 					collision_removal = true,
 	// 					vertical = false,
 	// 					node = {name= name.."particletexture"},
-	// 					--texture = "eat_particles_1.png"
+	// 					//texture = "eat_particles_1.png"
 	// 				})
-	// 				minetest.sound_play("armor_fall_damage", {object=player, gain = 1.0, max_hear_distance = 60,pitch = math.random(80,100)/100})
+	// 				core.sound_play("armor_fall_damage", {object=player, gain = 1.0, max_hear_distance = 60,pitch = math.random(80,100)/100})
 	// 			end
 	// 			hp_change = hp_change + absorption
 	// 			if hp_change >= 0 then
 	// 				hp_change = 0
 	// 			else
 	// 				player:set_hp(player:get_hp()+hp_change,{reason="correction"})
-	// 				minetest.sound_play("hurt", {object=player, gain = 1.0, max_hear_distance = 60,pitch = math.random(80,100)/100})
+	// 				core.sound_play("hurt", {object=player, gain = 1.0, max_hear_distance = 60,pitch = math.random(80,100)/100})
 	// 			end
 	// 		else
 	// 			player:set_hp(player:get_hp()+hp_change,{reason="correction"})
-	// 			minetest.sound_play("hurt", {object=player, gain = 1.0, max_hear_distance = 60,pitch = math.random(80,100)/100})
+	// 			core.sound_play("hurt", {object=player, gain = 1.0, max_hear_distance = 60,pitch = math.random(80,100)/100})
 	// 		end
 	// 	end
 	// end
@@ -103,17 +95,17 @@ namespace playerMechanics {
 	// local old_vel
 	// local damage_calc
 	// local pos
-	// minetest.register_globalstep(function(dtime)
-	// 	for _,player in ipairs(minetest.get_connected_players()) do
+	// core.register_globalstep(function(dtime)
+	// 	for _,player in ipairs(core.get_connected_players()) do
 	// 		name = player:get_player_name()
 	// 		old_vel = pool[name]
 	// 		if old_vel then
 	// 			new_vel = player:get_player_velocity().y
 	// 			if old_vel < -15 and new_vel >= -0.5 then
-	// 				--don't do fall damage on unloaded areas
+	// 				//don't do fall damage on unloaded areas
 	// 				pos = player:get_pos()
 	// 				pos.y = pos.y - 1
-	// 				if minetest.get_node_or_nil(pos) then
+	// 				if core.get_node_or_nil(pos) then
 	// 					calc_fall_damage(player,math.ceil(old_vel+14))
 	// 				end
 	// 			end
