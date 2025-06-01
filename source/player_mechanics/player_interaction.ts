1b3c2a7d3f03d808679dcede85f6c1d2090412a4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-01 06:33:43 -0400

    long

--- source/player_mechanics/player_interaction.ts
+++ source/player_mechanics/player_interaction.ts
@@ -161,34 +161,52 @@ namespace playerMechanics {
 		) => {
 			const old: string = itemstack.get_name();
 			// Pass through to check.
-			// 	core.after(0,function(pos, newnode, placer, oldnode, itemstack, pointed_thing,old)
-			// 		if not placer then
-			// 			return
-			// 		end
-			// 		new = placer:get_wielded_item():get_name()
-			// 		if old ~= new and new == "" then
-			// 			inv = placer:get_inventory()
-			// 			//check if another stack
-			// 			if inv:contains_item("main", old) then
-			// 				//print("moving stack")
-			// 				//run through inventory
-			// 				for i = 1,inv:get_size("main") do
-			// 					//if found set wielded item and remove old stack
-			// 					if inv:get_stack("main", i):get_name() == old then
-			// 						count = inv:get_stack("main", i):get_count()
-			// 						placer:set_wielded_item(old.." "..count)
-			// 						inv:set_stack("main",i,ItemStack(""))
-			// 						play_sound("pickup", {
-			// 							  to_player = player,
-			// 							  gain = 0.7,
-			// 							  pitch = random(60,100)/100
-			// 						})
-			// 						return
-			// 					end
-			// 				end
-			// 			end
-			// 		end
-			// 	end,pos, newnode, placer, oldnode, itemstack, pointed_thing,old)
+			core.after(
+				0,
+				(
+					pos,
+					newnode,
+					placer,
+					oldnode,
+					itemstack,
+					pointed_thing,
+					old
+				) => {
+					// 		if not placer then
+					// 			return
+					// 		end
+					// 		new = placer:get_wielded_item():get_name()
+					// 		if old ~= new and new == "" then
+					// 			inv = placer:get_inventory()
+					// 			//check if another stack
+					// 			if inv:contains_item("main", old) then
+					// 				//print("moving stack")
+					// 				//run through inventory
+					// 				for i = 1,inv:get_size("main") do
+					// 					//if found set wielded item and remove old stack
+					// 					if inv:get_stack("main", i):get_name() == old then
+					// 						count = inv:get_stack("main", i):get_count()
+					// 						placer:set_wielded_item(old.." "..count)
+					// 						inv:set_stack("main",i,ItemStack(""))
+					// 						play_sound("pickup", {
+					// 							  to_player = player,
+					// 							  gain = 0.7,
+					// 							  pitch = random(60,100)/100
+					// 						})
+					// 						return
+					// 					end
+					// 				end
+					// 			end
+					// 		end
+				},
+				pos,
+				newnode,
+				placer,
+				oldnode,
+				itemstack,
+				pointed_thing,
+				old
+			);
 		}
 	);
 
