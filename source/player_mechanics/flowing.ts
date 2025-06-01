550355290d0970bb5478988c38af56052682dfbc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-01 04:05:33 -0400

    Update flowing.ts

--- source/player_mechanics/flowing.ts
+++ source/player_mechanics/flowing.ts
@@ -13,44 +13,47 @@ namespace playerMechanics {
 	// local flow_dir
 	// local name
 
-	const pool = new Map<string, number>();
+	const pool = new Map<string, Vec3>();
 
 	core.register_globalstep(() => {
 		for (const [_, player] of ipairs(core.get_connected_players())) {
 			const flow_dir = flowLib.flow(player.get_pos());
 			const name: string = player.get_player_name();
 			if (flow_dir != null) {
-				//             //buffer continuation
-				//             if pool[name] then
-				//                 c_flow = pool[name]
-				//                 vel = player:get_player_velocity()
-				//                 acceleration = nil
-				//                 if c_flow.x ~= 0 then
-				//                     acceleration = vector.new(c_flow.x,0,0)
-				//                 elseif c_flow.z ~= 0 then
-				//                     acceleration = vector.new(0,0,c_flow.z)
-				//                 end
-				//                 acceleration = vector.multiply(acceleration, 0.075)
-				//                 player:add_player_velocity(acceleration)
-				//                 newvel = player:get_player_velocity()
-				//                 if newvel.x ~= 0 or newvel.z ~= 0 then
-				//                     return
-				//                 else
-				//                     pool[name] = nil
-				//                 end
-				//             else
-				//                 flow_dir = vector.multiply(flow_dir,10)
-				//                 vel = player:get_player_velocity()
-				//                 acceleration = nil
-				//                 if flow_dir.x ~= 0 then
-				//                     acceleration = vector.new(flow_dir.x,0,0)
-				//                 elseif flow_dir.z ~= 0 then
-				//                     acceleration = vector.new(0,0,flow_dir.z)
-				//                 end
-				//                 acceleration = vector.multiply(acceleration, 0.075)
-				//                 player:add_player_velocity(acceleration)
-				//                 pool[name] = flow_dir
-				//             end
+				// Buffer continuation.
+
+				let data: Vec3 | undefined = pool.get(name);
+
+				if (data != null) {
+					//                 c_flow = pool[name]
+					//                 vel = player:get_player_velocity()
+					//                 acceleration = nil
+					//                 if c_flow.x ~= 0 then
+					//                     acceleration = vector.new(c_flow.x,0,0)
+					//                 elseif c_flow.z ~= 0 then
+					//                     acceleration = vector.new(0,0,c_flow.z)
+					//                 end
+					//                 acceleration = vector.multiply(acceleration, 0.075)
+					//                 player:add_player_velocity(acceleration)
+					//                 newvel = player:get_player_velocity()
+					//                 if newvel.x ~= 0 or newvel.z ~= 0 then
+					//                     return
+					//                 else
+					//                     pool[name] = nil
+					//                 end
+				} else {
+					//                 flow_dir = vector.multiply(flow_dir,10)
+					//                 vel = player:get_player_velocity()
+					//                 acceleration = nil
+					//                 if flow_dir.x ~= 0 then
+					//                     acceleration = vector.new(flow_dir.x,0,0)
+					//                 elseif flow_dir.z ~= 0 then
+					//                     acceleration = vector.new(0,0,flow_dir.z)
+					//                 end
+					//                 acceleration = vector.multiply(acceleration, 0.075)
+					//                 player:add_player_velocity(acceleration)
+					//                 pool[name] = flow_dir
+				}
 			} else {
 				pool.delete(name);
 			}
