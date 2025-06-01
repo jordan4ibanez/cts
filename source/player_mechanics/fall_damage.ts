d046d40c5988debf1b9d793d00f00423623d7042
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-01 03:04:33 -0400

    Update fall_damage.ts

--- source/player_mechanics/fall_damage.ts
+++ source/player_mechanics/fall_damage.ts
@@ -49,63 +49,63 @@ namespace playerMechanics {
 		return false;
 	}
 
-	// local function calc_fall_damage(player,hp_change)
-	// 	if cancel_fall_damage(player) then
-	// 		return
-	// 	else
-	// 		local inv = player:get_inventory()
-	// 		local stack = inv:get_stack("armor_feet", 1)
-	// 		local name = stack:get_name()
-	// 		if name ~= "" then
-	// 			local absorption = 0
-	// 			absorption = core.get_item_group(name,"armor_level")*2
-	// 			//print("absorbtion:",absorption)
-	// 			local wear_level = ((9-core.get_item_group(name,"armor_level"))*8)*(5-core.get_item_group(name,"armor_type"))*math.abs(hp_change)
-	// 			stack:add_wear(wear_level)
-	// 			inv:set_stack("armor_feet", 1, stack)
-	// 			local new_stack = inv:get_stack("armor_feet",1):get_name()
-	// 			if new_stack == "" then
-	// 				core.sound_play("armor_break",{to_player=player:get_player_name(),gain=1,pitch=math.random(80,100)/100})
-	// 				recalculate_armor(player)
-	// 				set_armor_gui(player)
-	// 				//do particles too
-	// 			elseif core.get_item_group(new_stack,"boots") > 0 then
-	// 				local pos = player:get_pos()
-	// 				core.add_particlespawner({
-	// 					amount = 30,
-	// 					time = 0.00001,
-	// 					minpos = {x=pos.x-0.5, y=pos.y+0.1, z=pos.z-0.5},
-	// 					maxpos = {x=pos.x+0.5, y=pos.y+0.1, z=pos.z+0.5},
-	// 					minvel = vector.new(-0.5,1,-0.5),
-	// 					maxvel = vector.new(0.5 ,2 ,0.5),
-	// 					minacc = {x=0, y=-9.81, z=1},
-	// 					maxacc = {x=0, y=-9.81, z=1},
-	// 					minexptime = 0.5,
-	// 					maxexptime = 1.5,
-	// 					minsize = 0,
-	// 					maxsize = 0,
-	// 					//attached = player,
-	// 					collisiondetection = true,
-	// 					collision_removal = true,
-	// 					vertical = false,
-	// 					node = {name= name.."particletexture"},
-	// 					//texture = "eat_particles_1.png"
-	// 				})
-	// 				core.sound_play("armor_fall_damage", {object=player, gain = 1.0, max_hear_distance = 60,pitch = math.random(80,100)/100})
-	// 			end
-	// 			hp_change = hp_change + absorption
-	// 			if hp_change >= 0 then
-	// 				hp_change = 0
-	// 			else
-	// 				player:set_hp(player:get_hp()+hp_change,{reason="correction"})
-	// 				core.sound_play("hurt", {object=player, gain = 1.0, max_hear_distance = 60,pitch = math.random(80,100)/100})
-	// 			end
-	// 		else
-	// 			player:set_hp(player:get_hp()+hp_change,{reason="correction"})
-	// 			core.sound_play("hurt", {object=player, gain = 1.0, max_hear_distance = 60,pitch = math.random(80,100)/100})
-	// 		end
-	// 	end
-	// end
+	function calc_fall_damage(player: ObjectRef, hp_change: number) {
+		// 	if cancel_fall_damage(player) then
+		// 		return
+		// 	else
+		// 		local inv = player:get_inventory()
+		// 		local stack = inv:get_stack("armor_feet", 1)
+		// 		local name = stack:get_name()
+		// 		if name ~= "" then
+		// 			local absorption = 0
+		// 			absorption = core.get_item_group(name,"armor_level")*2
+		// 			//print("absorbtion:",absorption)
+		// 			local wear_level = ((9-core.get_item_group(name,"armor_level"))*8)*(5-core.get_item_group(name,"armor_type"))*math.abs(hp_change)
+		// 			stack:add_wear(wear_level)
+		// 			inv:set_stack("armor_feet", 1, stack)
+		// 			local new_stack = inv:get_stack("armor_feet",1):get_name()
+		// 			if new_stack == "" then
+		// 				core.sound_play("armor_break",{to_player=player:get_player_name(),gain=1,pitch=math.random(80,100)/100})
+		// 				recalculate_armor(player)
+		// 				set_armor_gui(player)
+		// 				//do particles too
+		// 			elseif core.get_item_group(new_stack,"boots") > 0 then
+		// 				local pos = player:get_pos()
+		// 				core.add_particlespawner({
+		// 					amount = 30,
+		// 					time = 0.00001,
+		// 					minpos = {x=pos.x-0.5, y=pos.y+0.1, z=pos.z-0.5},
+		// 					maxpos = {x=pos.x+0.5, y=pos.y+0.1, z=pos.z+0.5},
+		// 					minvel = vector.new(-0.5,1,-0.5),
+		// 					maxvel = vector.new(0.5 ,2 ,0.5),
+		// 					minacc = {x=0, y=-9.81, z=1},
+		// 					maxacc = {x=0, y=-9.81, z=1},
+		// 					minexptime = 0.5,
+		// 					maxexptime = 1.5,
+		// 					minsize = 0,
+		// 					maxsize = 0,
+		// 					//attached = player,
+		// 					collisiondetection = true,
+		// 					collision_removal = true,
+		// 					vertical = false,
+		// 					node = {name= name.."particletexture"},
+		// 					//texture = "eat_particles_1.png"
+		// 				})
+		// 				core.sound_play("armor_fall_damage", {object=player, gain = 1.0, max_hear_distance = 60,pitch = math.random(80,100)/100})
+		// 			end
+		// 			hp_change = hp_change + absorption
+		// 			if hp_change >= 0 then
+		// 				hp_change = 0
+		// 			else
+		// 				player:set_hp(player:get_hp()+hp_change,{reason="correction"})
+		// 				core.sound_play("hurt", {object=player, gain = 1.0, max_hear_distance = 60,pitch = math.random(80,100)/100})
+		// 			end
+		// 		else
+		// 			player:set_hp(player:get_hp()+hp_change,{reason="correction"})
+		// 			core.sound_play("hurt", {object=player, gain = 1.0, max_hear_distance = 60,pitch = math.random(80,100)/100})
+		// 		end
+		// 	end
+	}
 
 	// local pool = {}
 	// local name
@@ -113,7 +113,7 @@ namespace playerMechanics {
 	// local old_vel
 	// local damage_calc
 	// local pos
-    
+
 	// core.register_globalstep(function(dtime)
 	// 	for _,player in ipairs(core.get_connected_players()) do
 	// 		name = player:get_player_name()
