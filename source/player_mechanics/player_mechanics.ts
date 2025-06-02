67be108ca057da0bcbf057c8f048ecab6f21f3fc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-02 01:22:58 -0400

    Update player_mechanics.ts

--- source/player_mechanics/player_mechanics.ts
+++ source/player_mechanics/player_mechanics.ts
@@ -141,96 +141,99 @@ namespace playerMechanics {
 	}
 
 	// Controls player states.
-	function control_state (player: ObjectRef) {
-	// 	if get_if_player_attached(player) then
-	// 		return
-	// 	end
-	// 	hunger    = get_player_hunger(player)
-	// 	name      = player:get_player_name()
-	// 	temp_pool = pool[name]
-	// 	// water movement data
-	// 	head = core.get_item_group(get_player_head_env(player),"water") > 0
-	// 	legs = core.get_item_group(get_player_legs_env(player),"water") > 0
-	// 	in_water = temp_pool.swimming
-	// 	//check if in water
-	// 	if head then
-	// 		in_water = true
-	// 		temp_pool.swimming = true
-	// 	elseif temp_pool.swimming == true then
-	// 		swim_unlock = player_swim_under_check(player)
-	// 		swim_bump = player_swim_check(player)
-	// 		if swim_unlock then
-	// 			in_water = false
-	// 			temp_pool.swimming = false
-	// 			temp_pool.swim_bumped = core.get_us_time()/1000000
-	// 		elseif swim_bump and core.get_us_time()/1000000-temp_pool.swim_bumped > 1 then
-	// 			if player:get_player_velocity().y <= 0 then
-	// 				temp_pool.swim_bumped = core.get_us_time()/1000000
-	// 				player:add_player_velocity(vector.new(0,9,0))
-	// 			end
-	// 		end
-	// 	end
-	// 	if (in_water ~= temp_pool.was_in_water) or
-	// 	(temp_pool.state ~= temp_pool.old_state) or
-	// 	((temp_pool.state == 1 or temp_pool.state == 2) and hunger <= 6) then
-	// 		if (not in_water and temp_pool.was_in_water) then
-	// 			player:set_physics_override({
-	// 				sneak   = true,
-	// 			})
-	// 			force_update_animation(player)
-	// 			player:set_properties({
-	// 				collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3},
-	// 			})
-	// 		elseif in_water and not temp_pool.was_in_water then
-	// 			player:set_physics_override({
-	// 				sneak   = false,
-	// 			})
-	// 			force_update_animation(player)
-	// 			player:set_properties({
-	// 				collisionbox = {-0.3, 0.8, -0.3, 0.3, 1.6, 0.3},
-	// 			})
-	// 			player:set_eye_offset({x=0,y=0,z=0},{x=0,y=0,z=0})
-	// 		end
-	// 		// running/swimming fov modifier
-	// 		if hunger > 6 and (temp_pool.state == 1 or temp_pool.state == 2) then
-	// 			player:set_fov(1.25, true, 0.15)
-	// 			if temp_pool.state == 2 then
-	// 				player:set_physics_override({speed=1.75})
-	// 			elseif temp_pool.state == 1 then
-	// 				player:set_physics_override({speed=1.5})
-	// 			end
-	// 		elseif (not in_water and temp_pool.state ~= 1 and temp_pool.state ~= 2 and
-	// 		(temp_pool.old_state == 1 or temp_pool.old_state == 2)) or
-	// 		(in_water and temp_pool.state ~= 1 and temp_pool.state ~= 2 and temp_pool.state ~= 3 and
-	// 		(temp_pool.old_state == 1 or temp_pool.old_state == 2 or temp_pool.old_state == 3))then
-	// 			player:set_fov(1, true,0.15)
-	// 			player:set_physics_override({speed=1})
-	// 			send_running_cancellation(player,temp_pool.state==3) //preserve network data
-	// 		elseif (temp_pool.state == 1 or temp_pool.state == 2) and hunger <= 6 then
-	// 			player:set_fov(1, true,0.15)
-	// 			player:set_physics_override({speed=1})
-	// 			send_running_cancellation(player,false) //preserve network data
-	// 		end
-	// 		//sneaking
-	// 		if temp_pool.state == 3 and in_water then
-	// 			//send_running_cancellation(player,false)
-	// 		elseif not in_water and temp_pool.state == 3 and temp_pool.old_state ~= 3 then
-	// 			player:set_eye_offset({x=0,y=-1,z=0},{x=0,y=0,z=0})
-	// 		elseif not in_water and temp_pool.old_state == 3 and temp_pool.state ~= 3 then
-	// 			player:set_eye_offset({x=0,y=0,z=0},{x=0,y=0,z=0})
-	// 		end
-	// 		temp_pool.old_state    = state
-	// 		temp_pool.was_in_water = in_water
-	// 	// water movement
-	// 	elseif in_water then
-	// 		if not temp_pool.was_in_water then
-	// 			player:set_physics_override({
-	// 				sneak   = false ,
-	// 			})
-	// 		end
-	// 		temp_pool.old_state    = temp_pool.old_state
-	// 		temp_pool.was_in_water = in_water
-	// 	end
+	function control_state(player: ObjectRef) {
+		// todo: this depends on crafter player api
+		// if get_if_player_attached(player) {
+		// 	return
+		// }
+
+		// todo: this depends on crafter hunger
+		// const hunger    = get_player_hunger(player)
+		const name: string = player.get_player_name();
+		// 	temp_pool = pool[name]
+		// 	// water movement data
+		// 	head = core.get_item_group(get_player_head_env(player),"water") > 0
+		// 	legs = core.get_item_group(get_player_legs_env(player),"water") > 0
+		// 	in_water = temp_pool.swimming
+		// 	//check if in water
+		// 	if head then
+		// 		in_water = true
+		// 		temp_pool.swimming = true
+		// 	elseif temp_pool.swimming == true then
+		// 		swim_unlock = player_swim_under_check(player)
+		// 		swim_bump = player_swim_check(player)
+		// 		if swim_unlock then
+		// 			in_water = false
+		// 			temp_pool.swimming = false
+		// 			temp_pool.swim_bumped = core.get_us_time()/1000000
+		// 		elseif swim_bump and core.get_us_time()/1000000-temp_pool.swim_bumped > 1 then
+		// 			if player:get_player_velocity().y <= 0 then
+		// 				temp_pool.swim_bumped = core.get_us_time()/1000000
+		// 				player:add_player_velocity(vector.new(0,9,0))
+		// 			end
+		// 		end
+		// 	end
+		// 	if (in_water ~= temp_pool.was_in_water) or
+		// 	(temp_pool.state ~= temp_pool.old_state) or
+		// 	((temp_pool.state == 1 or temp_pool.state == 2) and hunger <= 6) then
+		// 		if (not in_water and temp_pool.was_in_water) then
+		// 			player:set_physics_override({
+		// 				sneak   = true,
+		// 			})
+		// 			force_update_animation(player)
+		// 			player:set_properties({
+		// 				collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3},
+		// 			})
+		// 		elseif in_water and not temp_pool.was_in_water then
+		// 			player:set_physics_override({
+		// 				sneak   = false,
+		// 			})
+		// 			force_update_animation(player)
+		// 			player:set_properties({
+		// 				collisionbox = {-0.3, 0.8, -0.3, 0.3, 1.6, 0.3},
+		// 			})
+		// 			player:set_eye_offset({x=0,y=0,z=0},{x=0,y=0,z=0})
+		// 		end
+		// 		// running/swimming fov modifier
+		// 		if hunger > 6 and (temp_pool.state == 1 or temp_pool.state == 2) then
+		// 			player:set_fov(1.25, true, 0.15)
+		// 			if temp_pool.state == 2 then
+		// 				player:set_physics_override({speed=1.75})
+		// 			elseif temp_pool.state == 1 then
+		// 				player:set_physics_override({speed=1.5})
+		// 			end
+		// 		elseif (not in_water and temp_pool.state ~= 1 and temp_pool.state ~= 2 and
+		// 		(temp_pool.old_state == 1 or temp_pool.old_state == 2)) or
+		// 		(in_water and temp_pool.state ~= 1 and temp_pool.state ~= 2 and temp_pool.state ~= 3 and
+		// 		(temp_pool.old_state == 1 or temp_pool.old_state == 2 or temp_pool.old_state == 3))then
+		// 			player:set_fov(1, true,0.15)
+		// 			player:set_physics_override({speed=1})
+		// 			send_running_cancellation(player,temp_pool.state==3) //preserve network data
+		// 		elseif (temp_pool.state == 1 or temp_pool.state == 2) and hunger <= 6 then
+		// 			player:set_fov(1, true,0.15)
+		// 			player:set_physics_override({speed=1})
+		// 			send_running_cancellation(player,false) //preserve network data
+		// 		end
+		// 		//sneaking
+		// 		if temp_pool.state == 3 and in_water then
+		// 			//send_running_cancellation(player,false)
+		// 		elseif not in_water and temp_pool.state == 3 and temp_pool.old_state ~= 3 then
+		// 			player:set_eye_offset({x=0,y=-1,z=0},{x=0,y=0,z=0})
+		// 		elseif not in_water and temp_pool.old_state == 3 and temp_pool.state ~= 3 then
+		// 			player:set_eye_offset({x=0,y=0,z=0},{x=0,y=0,z=0})
+		// 		end
+		// 		temp_pool.old_state    = state
+		// 		temp_pool.was_in_water = in_water
+		// 	// water movement
+		// 	elseif in_water then
+		// 		if not temp_pool.was_in_water then
+		// 			player:set_physics_override({
+		// 				sneak   = false ,
+		// 			})
+		// 		end
+		// 		temp_pool.old_state    = temp_pool.old_state
+		// 		temp_pool.was_in_water = in_water
+		// 	end
 	}
 
 	// core.register_globalstep(function(dtime)
