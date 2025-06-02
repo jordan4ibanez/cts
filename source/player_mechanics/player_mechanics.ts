c2d599f2875a6de46d4b4506d0a5fec0a103cdff
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-02 02:20:13 -0400

    Update player_mechanics.ts

--- source/player_mechanics/player_mechanics.ts
+++ source/player_mechanics/player_mechanics.ts
@@ -229,9 +229,10 @@ namespace playerMechanics {
 				(in_water && data.state != 1 && data.state != 2 && data.state != 3 &&
 				(data.old_state == 1 || data.old_state == 2 || data.old_state == 3))) {
 
-		// 			player:set_fov(1, true,0.15)
-		// 			player:set_physics_override({speed=1})
-		// 			send_running_cancellation(player,temp_pool.state==3) //preserve network data
+					player.set_fov(1, true,0.15)
+					player.set_physics_override({speed:1})
+					// Preserve network data.
+					send_running_cancellation(player,data.state==3) 
 				} else if ((data.state == 1 || data.state == 2) && hunger <= 6) {
 		// 			player:set_fov(1, true,0.15)
 		// 			player:set_physics_override({speed=1})
