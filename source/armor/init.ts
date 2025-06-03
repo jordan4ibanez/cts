36c270eb30406abe2bbd4029b2c3a9d1d9655382
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-03 08:16:33 -0400

    Update init.ts

--- source/armor/init.ts
+++ source/armor/init.ts
@@ -314,14 +314,22 @@ namespace armor {
 					],
 				});
 			} else if (armor_id == "chestplate") {
-				//             core.register_craft({
-				//                 output = "armor:"+material_id+"_"+armor_id,
-				//                 recipe = {
-				//                     {"main:"+material_id, ""                  , "main:"+material_id},
-				//                     {"main:"+material_id, "main:"+material_id, "main:"+material_id},
-				//                     {"main:"+material_id, "main:"+material_id, "main:"+material_id}
-				//                 }
-				//             })
+				core.register_craft({
+					output: "armor:" + material_id + "_" + armor_id,
+					recipe: [
+						["main:" + material_id, "", "main:" + material_id],
+						[
+							"main:" + material_id,
+							"main:" + material_id,
+							"main:" + material_id,
+						],
+						[
+							"main:" + material_id,
+							"main:" + material_id,
+							"main:" + material_id,
+						],
+					],
+				});
 			} else if (armor_id == "leggings") {
 				//             core.register_craft({
 				//                 output = "armor:"+material_id+"_"+armor_id,
