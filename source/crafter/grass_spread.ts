6036d055ac30c3ba635b7083afd21d542fd14f6b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-03 00:13:19 -0400

    Update grass_spread.ts

--- source/crafter/grass_spread.ts
+++ source/crafter/grass_spread.ts
@@ -1,18 +1,18 @@
 // Grass spread abm.
-namespace crafter{
-core.register_abm({
-	label: "Grass Grow",
-	nodenames: ["crafter:dirt"],
-	neighbors: ["crafter:grass", "air"],
-	interval: 10,
-	chance: 1000,
-	action: (pos: Vec3) => {
-		const light: number | null = core.get_node_light(pos, null);
-		// print(light)
-		if (!light || light < 10) {
-			return;
-		}
-		core.set_node(pos, { name: "crafter:grass" });
-	},
-});
+namespace crafter {
+	core.register_abm({
+		label: "Grass Grow",
+		nodenames: ["crafter:dirt"],
+		neighbors: ["crafter:grass", "air"],
+		interval: 10,
+		chance: 1000,
+		action: (pos: Vec3) => {
+			const light: number | null = core.get_node_light(pos, null);
+			// print(light)
+			if (!light || light < 10) {
+				return;
+			}
+			core.set_node(pos, { name: "crafter:grass" });
+		},
+	});
 }
