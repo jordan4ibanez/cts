1a2d89db981de695bf6b283cecb3fcdd344e914c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-03 00:13:49 -0400

    Update lava_cooling.ts

--- source/crafter/lava_cooling.ts
+++ source/crafter/lava_cooling.ts
@@ -1,25 +1,25 @@
 namespace crafter {
-core.register_abm({
-	label: "Lava cooling",
-	nodenames: ["crafter:lava"],
-	neighbors: ["crafter:water", "crafter:waterflow"],
-	interval: 1.0,
-	chance: 5.0,
-	catch_up: false,
-	action: (pos: Vec3) => {
-		core.set_node(pos, { name: "nether:obsidian" });
-	},
-});
+	core.register_abm({
+		label: "Lava cooling",
+		nodenames: ["crafter:lava"],
+		neighbors: ["crafter:water", "crafter:waterflow"],
+		interval: 1.0,
+		chance: 5.0,
+		catch_up: false,
+		action: (pos: Vec3) => {
+			core.set_node(pos, { name: "nether:obsidian" });
+		},
+	});
 
-core.register_abm({
-	label: "Lava cooling",
-	nodenames: ["crafter:lavaflow"],
-	neighbors: ["crafter:water", "crafter:waterflow"],
-	interval: 1.0,
-	chance: 5.0,
-	catch_up: false,
-	action: (pos: Vec3) => {
-		core.set_node(pos, { name: "crafter:cobble" });
-	},
-});
+	core.register_abm({
+		label: "Lava cooling",
+		nodenames: ["crafter:lavaflow"],
+		neighbors: ["crafter:water", "crafter:waterflow"],
+		interval: 1.0,
+		chance: 5.0,
+		catch_up: false,
+		action: (pos: Vec3) => {
+			core.set_node(pos, { name: "crafter:cobble" });
+		},
+	});
 }
