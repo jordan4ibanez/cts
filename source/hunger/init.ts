108fb7852c8b586bdd8abf338376422c344994e1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-04 11:18:48 -0400

    Update init.ts

--- source/hunger/init.ts
+++ source/hunger/init.ts
@@ -1,7 +1,7 @@
 namespace hunger {
 	// local mod_storage = core.get_mod_storage()
 	// local pool        = {}
-	// -- loads data from mod storage
+	// // loads data from mod storage
 	// local name
 	// local temp_pool
 	// local load_data = function(player)
@@ -20,7 +20,7 @@ namespace hunger {
 	// 		temp_pool.exhaustion            = 0
 	// 	end
 	// end
-	// -- saves data to be utilized on next login
+	// // saves data to be utilized on next login
 	// local name
 	// local temp_pool
 	// local save_data = function(name)
@@ -35,13 +35,13 @@ namespace hunger {
 	// 	mod_storage:set_int(name.."h_save",1)
 	// 	pool[name] = nil
 	// end
-	// -- is used for shutdowns to save all data
+	// // is used for shutdowns to save all data
 	// local save_all = function()
 	// 	for name,_ in pairs(pool) do
 	// 		save_data(name)
 	// 	end
 	// end
-	// -- an easy translation pool
+	// // an easy translation pool
 	// local satiation_pool = {
 	// 	[0]   = 1,
 	// 	[0.5] = 3,
@@ -49,11 +49,11 @@ namespace hunger {
 	// 	[2]   = 8,
 	// 	[3]   = 1
 	// }
-	// -- ticks up the exhaustion when counting down satiation
+	// // ticks up the exhaustion when counting down satiation
 	// local tick_up_satiation = function(state,exhaustion)
 	// 	return(exhaustion + satiation_pool[state])
 	// end
-	// -- an easy translation pool
+	// // an easy translation pool
 	// local hunger_pool = {
 	// 	[0]   = 1,
 	// 	[0.5] = 2,
@@ -61,25 +61,25 @@ namespace hunger {
 	// 	[2]   = 4,
 	// 	[3]   = 1
 	// }
-	// -- ticks up the exhaustion when counting down hunger
+	// // ticks up the exhaustion when counting down hunger
 	// local tick_up_hunger = function(state,exhaustion)
 	// 	return(exhaustion + hunger_pool[state])
 	// end
-	// -- allows other mods to set hunger data
+	// // allows other mods to set hunger data
 	// local name
 	// get_player_hunger = function(player)
 	// 	name = player:get_player_name()
 	// 	return(pool[name].hunger)
 	// end
-	// -- saves specific users data for when they relog
+	// // saves specific users data for when they relog
 	// core.register_on_leaveplayer(function(player)
 	// 	save_data(player)
 	// end)
-	// -- save all data to mod storage on shutdown
+	// // save all data to mod storage on shutdown
 	// core.register_on_shutdown(function()
 	// 	save_all()
 	// end)
-	// -- create new data for hunger per player
+	// // create new data for hunger per player
 	// local name
 	// core.register_on_joinplayer(function(player)
 	// 	name = player:get_player_name()
@@ -103,7 +103,7 @@ namespace hunger {
 	// 		offset        = {x = 24*10, y= -(48 + 24 + 39)},
 	// 	})
 	// end)
-	// -- resets the players hunger settings to max
+	// // resets the players hunger settings to max
 	// local name
 	// local temp_pool
 	// core.register_on_respawnplayer(function(player)
@@ -129,35 +129,35 @@ namespace hunger {
 	// local drowning
 	// hunger_update = function()
 	// 	for _,player in ipairs(core.get_connected_players()) do
-	// 		--do not regen player's health if dead - this will be reused for 1up apples
+	// 		//do not regen player's health if dead - this will be reused for 1up apples
 	// 		if player:get_hp() > 0 then
 	// 			name = player:get_player_name()
 	// 			temp_pool = pool[name]
-	// 			--movement state
+	// 			//movement state
 	// 			state = get_player_state(player)
-	// 			-- if player is moving in state 0 add 0.5
+	// 			// if player is moving in state 0 add 0.5
 	// 			if state == 0 then
 	// 				input = player:get_player_control()
 	// 				if input.jump or input.right or input.left or input.down or input.up then
 	// 					state = 0.5
 	// 				end
 	// 			end
-	// 			-- count down invisible satiation bar
+	// 			// count down invisible satiation bar
 	// 			if temp_pool.satiation > 0 and temp_pool.hunger >= 20 then
 	// 				temp_pool.exhaustion = tick_up_satiation(state, temp_pool.exhaustion)
 	// 				if temp_pool.exhaustion > exhaustion_peak then
 	// 					temp_pool.satiation = temp_pool.satiation - 1
 	// 					temp_pool.exhaustion = temp_pool.exhaustion - exhaustion_peak
-	// 					--reset this to use for the hunger tick
+	// 					//reset this to use for the hunger tick
 	// 					if temp_pool.satiation == 0 then
 	// 						temp_pool.exhaustion = 0
 	// 					end
 	// 				end
-	// 			-- count down hunger bars
+	// 			// count down hunger bars
 	// 			elseif temp_pool.hunger > 0 then
 	// 				temp_pool.exhaustion = tick_up_hunger(state,temp_pool.exhaustion)
 	// 				if temp_pool.exhaustion >= hunger_peak then
-	// 					--don't allow hunger to go negative
+	// 					//don't allow hunger to go negative
 	// 					if temp_pool.hunger > 0 then
 	// 						temp_pool.exhaustion = temp_pool.exhaustion - hunger_peak
 	// 						temp_pool.hunger = temp_pool.hunger - 1
@@ -169,7 +169,7 @@ namespace hunger {
 	// 						data      =  temp_pool.hunger
 	// 					})
 	// 				end
-	// 			-- hurt the player if hunger bar empty
+	// 			// hurt the player if hunger bar empty
 	// 			elseif temp_pool.hunger <= 0 then
 	// 				temp_pool.exhaustion = temp_pool.exhaustion + 1
 	// 				hp = player:get_hp()
@@ -180,15 +180,15 @@ namespace hunger {
 	// 			end
 	// 			hp = player:get_hp()
 	// 			drowning = is_player_drowning(player)
-	// 			--make regeneration happen every second
-	// 			if not is_player_on_fire(player) and drowning == 0 and temp_pool.hunger >= 20 and hp < 20 then --  meta:get_int("on_fire") == 0
+	// 			//make regeneration happen every second
+	// 			if not is_player_on_fire(player) and drowning == 0 and temp_pool.hunger >= 20 and hp < 20 then //  meta:get_int("on_fire") == 0
 	// 				temp_pool.regeneration_interval = temp_pool.regeneration_interval + 1
 	// 				if temp_pool.regeneration_interval >= 2 then
 	// 					player:set_hp( hp + 1 )
 	// 					temp_pool.exhaustion = temp_pool.exhaustion + 32
 	// 					temp_pool.regeneration_interval = 0
 	// 				end
-	// 			--reset the regen interval
+	// 			//reset the regen interval
 	// 			else
 	// 				temp_pool.regeneration_interval = 0
 	// 			end
@@ -203,7 +203,7 @@ namespace hunger {
 	// 		hunger_update()
 	// 	end)
 	// end)
-	// --take away hunger and satiation randomly while mining
+	// //take away hunger and satiation randomly while mining
 	// local name
 	// core.register_on_dignode(function(pos, oldnode, digger)
 	// 	if digger and digger:is_player() then
@@ -211,14 +211,14 @@ namespace hunger {
 	// 		pool[name].exhaustion = pool[name].exhaustion + math.random(0,2)
 	// 	end
 	// end)
-	// -- take the eaten food
+	// // take the eaten food
 	// local item
 	// local take_food = function(player)
 	// 	item = player:get_wielded_item()
 	// 	item:take_item()
 	// 	player:set_wielded_item(item)
 	// end
-	// -- players eat food
+	// // players eat food
 	// local name
 	// local temp_pool
 	// local item
@@ -239,8 +239,8 @@ namespace hunger {
 	// 	if temp_pool.hunger > 20 then
 	// 		temp_pool.hunger = 20
 	// 	end
-	// 	-- unlimited
-	// 	-- this makes the game easier
+	// 	// unlimited
+	// 	// this makes the game easier
 	// 	temp_pool.satiation = temp_pool.satiation + satiation
 	// 	take_food(player)
 	// 	hud_manager.change_hud({
@@ -250,7 +250,7 @@ namespace hunger {
 	// 		data      =  temp_pool.hunger
 	// 	})
 	// end
-	// -- easily allows mods to register food
+	// // easily allows mods to register food
 	// core.register_food = function(name,def)
 	// 	core.register_craftitem(":"..name, {
 	// 		description = def.description,
