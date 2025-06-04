9fecaad4172c39081187967ddaa05566af6fea93
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-04 10:43:30 -0400

    what th JKFDAlkjfa

--- source/farming/tools.ts
+++ source/farming/tools.ts
@@ -212,10 +212,26 @@ namespace farming {
 
 				const tilled: boolean = till_soil(pointed_thing.under);
 				if (tilled == true) {
-									if (core.registered_nodes[core.get_node(vector.new(pointed_thing.under.x,pointed_thing.under.y+1,pointed_thing.under.z)).name].buildable_to) {
-					// 					core.dig_node(vector.new(pointed_thing.under.x,pointed_thing.under.y+1,pointed_thing.under.z))
-                                    }
-					// 				itemstack:add_wear(wear)
+					if (
+						core.registered_nodes[
+							core.get_node(
+								vector.create3d(
+									pointed_thing.under.x,
+									pointed_thing.under.y + 1,
+									pointed_thing.under.z
+								)
+							).name
+						]?.buildable_to
+					) {
+						core.dig_node(
+							vector.create3d(
+								pointed_thing.under.x,
+								pointed_thing.under.y + 1,
+								pointed_thing.under.z
+							)
+						);
+					}
+					itemstack.add_wear(wear);
 				}
 				// 			local damage = itemstack:get_wear()
 				// 			if damage <= 0 and tilled == true  then
