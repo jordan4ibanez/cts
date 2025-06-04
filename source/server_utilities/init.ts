399fab2633d90906b685480b0e0f43306be75bb5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-03 22:55:00 -0400

    Update init.ts

--- source/server_utilities/init.ts
+++ source/server_utilities/init.ts
@@ -28,7 +28,7 @@ namespace serverUtilities {
 		description:
 			"Use this to set your home. Can be returned to by setting /home",
 		privs: {},
-		func: (name) => {
+		func: (name: string) => {
 			const player: ObjectRef | null = core.get_player_by_name(name);
 
 			if (player == null) {
@@ -68,30 +68,32 @@ namespace serverUtilities {
 			}
 		},
 	});
-	// core.register_chatcommand("home", {
-	// 	params = "nil",
-	// 	description = "Use this to set your home. Can be returned to by setting /home",
-	// 	privs = {},
-	// 	func = function(name)
-	// 		local time = core.get_us_time()/1000000
-	// 		local player = core.get_player_by_name(name)
-	// 		if not pool[name] or pool[name] and time-pool[name] > home_timeout then
-	// 			local newpos = core.deserialize(mod_storage:get_string(name+":crafter_home"))
-	// 			if newpos then
-	// 				player:add_player_velocity(vector.multiply(player:get_player_velocity(),-1))
-	// 				player:move_to(newpos)
-	// 				pool[name] = time
-	// 			else
-	// 				core.chat_send_player(name, "No home set.")
-	// 			end
-	// 		elseif pool[name] then
-	// 			local diff = home_timeout-math.ceil(time-pool[name])+1
-	// 			local s = "s"
-	// 			if diff == 1 then
-	// 				s = ""
-	// 			end
-	// 			core.chat_send_player(name, diff+" more second"+s+" until you can run that command.")
-	// 		end
-	// 	end,
-	// })
+
+	core.register_chatcommand("home", {
+		params: "nil",
+		description:
+			"Use this to go to your home. You can do this by running /sethome",
+		privs: {},
+		func: (name: string) => {
+			// 		local time = core.get_us_time()/1000000
+			// 		local player = core.get_player_by_name(name)
+			// 		if not pool[name] or pool[name] and time-pool[name] > home_timeout then
+			// 			local newpos = core.deserialize(mod_storage:get_string(name+":crafter_home"))
+			// 			if newpos then
+			// 				player:add_player_velocity(vector.multiply(player:get_player_velocity(),-1))
+			// 				player:move_to(newpos)
+			// 				pool[name] = time
+			// 			else
+			// 				core.chat_send_player(name, "No home set.")
+			// 			end
+			// 		elseif pool[name] then
+			// 			local diff = home_timeout-math.ceil(time-pool[name])+1
+			// 			local s = "s"
+			// 			if diff == 1 then
+			// 				s = ""
+			// 			end
+			// 			core.chat_send_player(name, diff+" more second"+s+" until you can run that command.")
+			// 		end
+		},
+	});
 }
