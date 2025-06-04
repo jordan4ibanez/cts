2e01d29b244d86769e0c06e70b4601989d7ec1c7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-04 08:49:28 -0400

    crafter

--- source/farming/init.ts
+++ source/farming/init.ts
@@ -1,19 +1,19 @@
 namespace farming {
 	utility.loadFiles(["plant_api", "registers", "tools", "soil"]);
 
-	core.register_craftitem("farming:wheat", {
+	core.register_craftitem("crafter_farming:wheat", {
 		description: "Wheat",
 		inventory_image: "wheat_harvested.png",
 	});
 
     // Todo: depends on the crafter hunger mod.
-	// farming.register_food("farming:bread", {
+	// farming.register_food("crafter_farming:bread", {
 	// 	description: "Bread",
 	// 	texture: "bread.png",
 	// 	satiation: 6,
 	// 	hunger: 2,
 	// });
-	// farming.register_food("farming:toast", {
+	// farming.register_food("crafter_farming:toast", {
 	// 	description: "Toast",
 	// 	texture: "bread.png^[colorize:black:100",
 	// 	satiation: 12,
@@ -21,13 +21,13 @@ namespace farming {
 	// });
 
 	core.register_craft({
-		output: "farming:bread",
-		recipe: [["farming:wheat", "farming:wheat", "farming:wheat"]],
+		output: "crafter_farming:bread",
+		recipe: [["crafter_farming:wheat", "crafter_farming:wheat", "crafter_farming:wheat"]],
 	});
 	core.register_craft({
 		type: CraftRecipeType.cooking,
-		output: "farming:toast",
-		recipe: "farming:bread",
+		output: "crafter_farming:toast",
+		recipe: "crafter_farming:bread",
 		cooktime: 3,
 	});
 }
