4013c37b4baa21afeb3873d60b64998a1040b490
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-04 03:01:17 -0400

    Update init.ts

--- source/farming/init.ts
+++ source/farming/init.ts
@@ -1,32 +1,33 @@
 namespace farming {
 	utility.loadFiles(["plant_api", "registers", "tools", "soil"]);
 
-	// minetest.register_craftitem("farming:wheat", {
-	// 	description = "Wheat",
-	// 	inventory_image = "wheat_harvested.png",
-	// })
-	// minetest.register_food("farming:bread",{
-	// 	description = "Bread",
-	// 	texture = "bread.png",
-	// 	satiation=6,
-	// 	hunger=2,
-	// })
-	// minetest.register_food("farming:toast",{
-	// 	description = "Toast",
-	// 	texture = "bread.png^[colorize:black:100",
-	// 	satiation=12,
-	// 	hunger=4,
-	// })
-	// minetest.register_craft({
-	// 	output = "farming:bread",
-	// 	recipe = {
-	// 		{"farming:wheat", "farming:wheat", "farming:wheat"}
-	// 	}
-	// })
-	// minetest.register_craft({
-	// 	type = "cooking",
-	// 	output = "farming:toast",
-	// 	recipe = "farming:bread",
-	// 	cooktime = 3,
-	// })
+	core.register_craftitem("farming:wheat", {
+		description: "Wheat",
+		inventory_image: "wheat_harvested.png",
+	});
+
+    // Todo: depends on farming.
+	// farming.register_food("farming:bread", {
+	// 	description: "Bread",
+	// 	texture: "bread.png",
+	// 	satiation: 6,
+	// 	hunger: 2,
+	// });
+	// farming.register_food("farming:toast", {
+	// 	description: "Toast",
+	// 	texture: "bread.png^[colorize:black:100",
+	// 	satiation: 12,
+	// 	hunger: 4,
+	// });
+
+	core.register_craft({
+		output: "farming:bread",
+		recipe: [["farming:wheat", "farming:wheat", "farming:wheat"]],
+	});
+	core.register_craft({
+		type: CraftRecipeType.cooking,
+		output: "farming:toast",
+		recipe: "farming:bread",
+		cooktime: 3,
+	});
 }
