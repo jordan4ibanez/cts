69c447dc6ed26f6d55805e6b1572ecb3f8814a44
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-03 22:09:34 -0400

    Update init.ts

--- source/server_utilities/init.ts
+++ source/server_utilities/init.ts
@@ -1,34 +1,35 @@
 namespace serverUtilities {
-	// local minetest,os = minetest,os
-
 	const mod_storage: MetaRef = core.get_mod_storage();
 
 	// local pool = {}
-	// local home_timeout = 60
-	// //this does not terminate data because player's can spam
-	// //leave and come back in to reset the home timout
-	// core.register_chatcommand("sethome", {
-	// 	params = "nil",
-	// 	description = "Use this to set your home. Can be returned to by setting /home",
-	// 	privs = {},
-	// 	func = function(name)
-	// 		local time = core.get_us_time()/1000000
-	// 		local player = core.get_player_by_name(name)
-	// 		local pos = player:get_pos()
-	// 		if not pool[name] or pool[name] and time-pool[name] > home_timeout then
-	// 			mod_storage:set_string(name+"home", core.serialize(pos))
-	// 			pool[name] = time
-	// 			core.chat_send_player(name, "Home set.")
-	// 		elseif pool[name] then
-	// 			local diff = home_timeout-math.ceil(time-pool[name])+1
-	// 			local s = "s"
-	// 			if diff == 1 then
-	// 				s = ""
-	// 			end
-	// 			core.chat_send_player(name, diff+" more second"+s+" until you can run that command.")
-	// 		end
-	// 	end,
-	// })
+
+	const home_timeout: number = 60;
+
+	// This does not terminate data because player's can spam
+	// leave and come back in to reset the home timout.
+	core.register_chatcommand("sethome", {
+		params: "nil",
+		description:
+			"Use this to set your home. Can be returned to by setting /home",
+		privs: {},
+		func: (name) => {
+			// 		local time = core.get_us_time()/1000000
+			// 		local player = core.get_player_by_name(name)
+			// 		local pos = player:get_pos()
+			// 		if not pool[name] or pool[name] and time-pool[name] > home_timeout then
+			// 			mod_storage:set_string(name+"home", core.serialize(pos))
+			// 			pool[name] = time
+			// 			core.chat_send_player(name, "Home set.")
+			// 		elseif pool[name] then
+			// 			local diff = home_timeout-math.ceil(time-pool[name])+1
+			// 			local s = "s"
+			// 			if diff == 1 then
+			// 				s = ""
+			// 			end
+			// 			core.chat_send_player(name, diff+" more second"+s+" until you can run that command.")
+			// 		end
+		},
+	});
 	// core.register_chatcommand("home", {
 	// 	params = "nil",
 	// 	description = "Use this to set your home. Can be returned to by setting /home",
