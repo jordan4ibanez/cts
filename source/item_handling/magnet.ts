93fc34fdc44485fa6614659e62e53ec02a0c220f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-05 10:06:26 -0400

    Update magnet.ts

--- source/item_handling/magnet.ts
+++ source/item_handling/magnet.ts
@@ -83,7 +83,7 @@ namespace item_handling {
 					entity.trigger_collection_calculation();
 				}
 			} else if (entity.name == "crafter_experience:orb") {
-				print("collected??");
+				print("adding 2");
 				entity.collector = player.get_player_name();
 				entity.collected = true;
 			}
