77612fdd9afa5be957c5f63a23fe0c5903d9f940
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-05 08:54:28 -0400

    Update init.ts

--- source/experience/init.ts
+++ source/experience/init.ts
@@ -195,9 +195,12 @@ namespace experience {
 	});
 
 	function level_up_experience(player: ObjectRef): void {
-		// 	name = player:get_player_name()
-		// 	temp_pool = pool[name]
-		//     temp_pool.xp_level = temp_pool.xp_level + 1
+		const name: string = player.get_player_name();
+		const data: ExpData | undefined = pool.get(name);
+		if (data == null) {
+			throw new Error(`Player [${name}] was never added to the pool.`);
+		}
+		data.xp_level += 1;
 		// 	hud_manager.change_hud({
 		// 		player   = player,
 		// 		hud_name = "xp_level_fg",
