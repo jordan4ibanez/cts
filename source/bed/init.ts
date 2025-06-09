f92a73ec685b61f4fd88fbeb63e737e5efabaa3e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-09 19:49:33 -0400

    Update init.ts

--- source/bed/init.ts
+++ source/bed/init.ts
@@ -115,13 +115,14 @@ namespace bed {
 
 	let global_step_timer: number = 0;
 	core.register_globalstep((dtime: number) => {
-		// 	if sleep_loop then
+		if (!sleep_loop) {
+			return;
+		}
 		// 		global_step_timer = global_step_timer + dtime
 		// 		if global_step_timer > 0.25 then
 		// 			global_step_timer = 0
 		// 			global_sleep_check()
 		// 		end
-		// 	end
 	});
 	// // delete data on player leaving
 	// local name
