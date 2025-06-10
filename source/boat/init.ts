e7140c835e3ac45f595001fe7b69b9257e07b7b4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-10 12:55:47 -0400

    Update init.ts

--- source/boat/init.ts
+++ source/boat/init.ts
@@ -261,15 +261,19 @@ namespace boat {
 		}
 
 		flow(): void {
-			const flow_dir: Vec3 | null = flowLib.flow(this.object.get_pos());
+			let flow_dir: Vec3 | null = flowLib.flow(this.object.get_pos());
 			if (flow_dir == null) {
 				return;
 			}
-			// 	flow_dir = vector.multiply(flow_dir,10)
-			// 	local vel = this.object.get_velocity()
-			// 	local acceleration = vector.new(flow_dir.x-vel.x,flow_dir.y-vel.y,flow_dir.z-vel.z)
-			// 	acceleration = vector.multiply(acceleration, 0.01)
-			// 	this.object.add_velocity(acceleration)
+			flow_dir = vector.multiply(flow_dir, 10);
+			const vel: Vec3 = this.object.get_velocity();
+			let acceleration: Vec3 = vector.create3d(
+				flow_dir.x - vel.x,
+				flow_dir.y - vel.y,
+				flow_dir.z - vel.z
+			);
+			acceleration = vector.multiply(acceleration, 0.01);
+			this.object.add_velocity(acceleration);
 		}
 
 		// 	on_step = function(self, dtime)
