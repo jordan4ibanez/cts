cafa2ae70b0bcc28bb4baa135c7cdd584e2a88ba
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-10 12:53:00 -0400

    Update init.ts

--- source/boat/init.ts
+++ source/boat/init.ts
@@ -260,16 +260,17 @@ namespace boat {
 			this.lag_check = core.get_us_time() / 1000000;
 		}
 
-		// 	flow = function(self)
-		// 		local flow_dir = flow(this.object.get_pos())
-		// 		if flow_dir then
-		// 			flow_dir = vector.multiply(flow_dir,10)
-		// 			local vel = this.object.get_velocity()
-		// 			local acceleration = vector.new(flow_dir.x-vel.x,flow_dir.y-vel.y,flow_dir.z-vel.z)
-		// 			acceleration = vector.multiply(acceleration, 0.01)
-		// 			this.object.add_velocity(acceleration)
-		// 		end
-		// 	end,
+		flow(): void {
+			// 		local flow_dir = flow(this.object.get_pos())
+			// 		if flow_dir then
+			// 			flow_dir = vector.multiply(flow_dir,10)
+			// 			local vel = this.object.get_velocity()
+			// 			local acceleration = vector.new(flow_dir.x-vel.x,flow_dir.y-vel.y,flow_dir.z-vel.z)
+			// 			acceleration = vector.multiply(acceleration, 0.01)
+			// 			this.object.add_velocity(acceleration)
+			// 		end
+		}
+
 		// 	on_step = function(self, dtime)
 		// 		this.check_if_on_land(self)
 		// 		this.push(self)
