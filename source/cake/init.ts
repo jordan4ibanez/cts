a2a3f5aeebba2bbd89dc3a4e78ccb6282a83ed92
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-10 19:58:13 -0400

    Update init.ts

--- source/cake/init.ts
+++ source/cake/init.ts
@@ -10,54 +10,55 @@ namespace cake {
 		hunger: 6,
 	});
 
-	// for i = 0,13 do
-	// 	local missing_slice
-	// 	if i == 0 then
-	// 		missing_slice = "cake_side.png"
-	// 	else
-	// 		missing_slice = "cake_inner.png"
-	// 	end
-	// 	core.register_node("cake:cake_"+i, {
-	// 		description = "Cake",
-	// 		tiles = {
-	// 			"cake_top.png",
-	// 			"cake_bottom.png",
-	// 			"cake_side.png",
-	// 			"cake_side.png",
-	// 			missing_slice,
-	// 			"cake_side.png"
-	// 		},
-	// 		drawtype = "nodebox",
-	// 		paramtype = "light",
-	// 		node_box = {
-	// 			type = "fixed",
-	// 			fixed = {
-	// 				{-7/16, -8/16, -7/16, 7/16, -1/16, (7-i)/16}, // Cake
-	// 			}
-	// 		},
-	// 		drop = "",
-	// 		sounds = main.woolSound(),
-	// 		groups = {wool=1,cake=i,falling_node=1},
-	// 		on_construct = function(pos)
-	// 			//randomly cake eats itself
-	// 			if random() > 0.995 then
-	// 				set_node(pos, {name="cake:cursed_cake_0"})
-	// 			end
-	// 		end,
-	// 		on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
-	// 			player_eat_food(clicker,"cake:cake_item_placeholder")
-	// 			//clicker:set_hp(clicker:get_hp()+5)
-	// 			if i == 13 then
-	// 		        play_sound("eat_finish",{pos=pos,gain=0.2,pitch=random(90,100)/100})
-	// 		        core.remove_node(pos)
-	// 		        return
-	// 	        else
-	// 		        play_sound("eat",{pos=pos,gain=0.2,pitch=random(90,100)/100})
-	// 		        set_node(pos, {name="cake:cake_"+i+1})
-	// 	        end
-	// 		end,
-	// 	})
-	// end
+	for (const i of $range(0, 13)) {
+		// 	local missing_slice
+		// 	if i == 0 then
+		// 		missing_slice = "cake_side.png"
+		// 	else
+		// 		missing_slice = "cake_inner.png"
+		// 	end
+		// 	core.register_node("cake:cake_"+i, {
+		// 		description = "Cake",
+		// 		tiles = {
+		// 			"cake_top.png",
+		// 			"cake_bottom.png",
+		// 			"cake_side.png",
+		// 			"cake_side.png",
+		// 			missing_slice,
+		// 			"cake_side.png"
+		// 		},
+		// 		drawtype = "nodebox",
+		// 		paramtype = "light",
+		// 		node_box = {
+		// 			type = "fixed",
+		// 			fixed = {
+		// 				{-7/16, -8/16, -7/16, 7/16, -1/16, (7-i)/16}, // Cake
+		// 			}
+		// 		},
+		// 		drop = "",
+		// 		sounds = main.woolSound(),
+		// 		groups = {wool=1,cake=i,falling_node=1},
+		// 		on_construct = function(pos)
+		// 			//randomly cake eats itself
+		// 			if random() > 0.995 then
+		// 				set_node(pos, {name="cake:cursed_cake_0"})
+		// 			end
+		// 		end,
+		// 		on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+		// 			player_eat_food(clicker,"cake:cake_item_placeholder")
+		// 			//clicker:set_hp(clicker:get_hp()+5)
+		// 			if i == 13 then
+		// 		        play_sound("eat_finish",{pos=pos,gain=0.2,pitch=random(90,100)/100})
+		// 		        core.remove_node(pos)
+		// 		        return
+		// 	        else
+		// 		        play_sound("eat",{pos=pos,gain=0.2,pitch=random(90,100)/100})
+		// 		        set_node(pos, {name="cake:cake_"+i+1})
+		// 	        end
+		// 		end,
+		// 	})
+	}
+
 	// for i = 0,13 do
 	// 	local missing_slice
 	// 	if i == 0 then
