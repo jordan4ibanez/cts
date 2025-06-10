dd5632324b70d85486f731662ab78a6226d0eb14
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-09 20:05:42 -0400

    Update init.ts

--- source/bed/init.ts
+++ source/bed/init.ts
@@ -153,20 +153,19 @@ namespace bed {
 			core.chat_send_player(name, "You can only sleep at night");
 			return;
 		}
-		// 		local real_dir = core.facedir_to_dir(dir)
-		// 		player:add_player_velocity(vector.multiply(player:get_player_velocity(),-1))
-		// 		local new_pos = vector.subtract(pos,vector.divide(real_dir,2))
-		// 		player:move_to(new_pos)
-		// 		player:set_look_vertical(0)
-		// 		player:set_look_horizontal(yaw_translation[dir])
-		// 		core.show_formspec(name, "bed", bed_gui)
-		// 		player_is_sleeping(player,true)
-		// 		set_player_animation(player,"lay",0,false)
-		// 		player:set_eye_offset({x=0,y=-12,z=-7},{x=0,y=0,z=0})
-		// 		pool[name] = {pos=new_pos,sleeping=false}
-		// 		csm_send_player_to_sleep(player)
-		// 		sleep_loop = true
-		
+		// local real_dir = core.facedir_to_dir(dir)
+		// player:add_player_velocity(vector.multiply(player:get_player_velocity(),-1))
+		// local new_pos = vector.subtract(pos,vector.divide(real_dir,2))
+		// player:move_to(new_pos)
+		// player:set_look_vertical(0)
+		// player:set_look_horizontal(yaw_translation[dir])
+		// core.show_formspec(name, "bed", bed_gui)
+		// player_is_sleeping(player,true)
+		// set_player_animation(player,"lay",0,false)
+		// player:set_eye_offset({x=0,y=-12,z=-7},{x=0,y=0,z=0})
+		// pool[name] = {pos=new_pos,sleeping=false}
+		// csm_send_player_to_sleep(player)
+		// sleep_loop = true
 	}
 
 	// core.register_on_player_receive_fields(function(player, formname, fields)
