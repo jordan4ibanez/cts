356fc013b1036f0932b279b5b435dbe5c4f9baeb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-10 17:36:32 -0400

    Update init.ts

--- source/bow/init.ts
+++ source/bow/init.ts
@@ -213,7 +213,7 @@ namespace bow {
 				for (const [_, object] of ipairs(
 					get_objects_inside_radius(pos, 2)
 				)) {
-					if (!this.stuck  && ((object.is_player() && object.get_player_name() != this.owner && object.get_hp() > 0) || (object.get_luaentity() && (object.get_luaentity() as any?)?.mobname))) {
+					if (!this.stuck  && ((object.is_player() && object.get_player_name() != this.owner && object.get_hp() > 0) || (object.get_luaentity() && (object.get_luaentity() as any)?.mobname))) {
 					// 	object:punch(this.object, 2,
 					// 		{
 					// 		full_punch_interval=1.5,
@@ -221,7 +221,7 @@ namespace bow {
 					// 	})
 					// 	this.object.remove()
 					// 	return
-					 } elseif this.timer > 3 and (object:is_player() and object:get_player_name() == this.owner) {
+					 } else if (this.timer > 3 && (object.is_player() && object.get_player_name() == this.owner)) {
 					// 	this.collecting = true
 					// 	local inv = object:get_inventory()
 					// 	if inv and inv:room_for_item("main", ItemStack("bow:arrow")) then
