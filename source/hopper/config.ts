c4e9b4f1903deeecd18b5f54dc3e3d13d028d74c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-17 14:06:55 -0400

    Update config.ts

--- source/hopper/config.ts
+++ source/hopper/config.ts
@@ -1,17 +1,16 @@
-// hopper.config = {}
-
-// -- settings
-// hopper.config.texture_resolution = core.settings:get("hopper_texture_size")
-// if hopper.config.texture_resolution == nil then
-// 	hopper.config.texture_resolution = "16"
-// end
-
-// hopper.config.single_craftable_item = core.settings:get_bool("hopper_single_craftable_item")
-// if hopper.config.single_craftable_item == nil then
-// 	hopper.config.single_craftable_item = true
-// end
-
-// hopper.config.eject_button_enabled = core.settings:get_bool("hopper_eject_button")
-// if hopper.config.eject_button_enabled == nil then
-// 	hopper.config.eject_button_enabled = true
-// end
+namespace hopper {
+	// hopper.config = {}
+	// -- settings
+	// hopper.config.texture_resolution = core.settings:get("hopper_texture_size")
+	// if hopper.config.texture_resolution == nil then
+	// 	hopper.config.texture_resolution = "16"
+	// end
+	// hopper.config.single_craftable_item = core.settings:get_bool("hopper_single_craftable_item")
+	// if hopper.config.single_craftable_item == nil then
+	// 	hopper.config.single_craftable_item = true
+	// end
+	// hopper.config.eject_button_enabled = core.settings:get_bool("hopper_eject_button")
+	// if hopper.config.eject_button_enabled == nil then
+	// 	hopper.config.eject_button_enabled = true
+	// end
+}
