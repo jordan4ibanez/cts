33b92881d61e254ffb1483465eae5fe30ab5ec89
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-17 14:25:53 -0400

    Update config.ts

--- source/hopper/config.ts
+++ source/hopper/config.ts
@@ -1,10 +1,3 @@
 namespace hopper {
-	// This is currently being used as a crutch to fix this mod.
-	export class config {
-		private constructor() {}
-
-		static readonly texture_resolution: string = "16";
-		static readonly single_craftable_item: boolean = true;
-		static readonly eject_button_enabled: boolean = true;
-	}
+	
 }
