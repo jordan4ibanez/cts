6c0d8f295764c5b4d0c8151ad57bdccb0c2b7326
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-17 14:25:22 -0400

    Update config.ts

--- source/hopper/config.ts
+++ source/hopper/config.ts
@@ -6,15 +6,7 @@ namespace hopper {
 		private constructor() {}
 
 		static readonly texture_resolution: string = "16";
+		static readonly single_craftable_item: boolean = true;
+		static readonly eject_button_enabled: boolean = true;
 	}
-
-	// hopper.config.single_craftable_item = core.settings:get_bool("hopper_single_craftable_item")
-	// if hopper.config.single_craftable_item == nil then
-	// 	hopper.config.single_craftable_item = true
-	// end
-
-	// hopper.config.eject_button_enabled = core.settings:get_bool("hopper_eject_button")
-	// if hopper.config.eject_button_enabled == nil then
-	// 	hopper.config.eject_button_enabled = true
-	// end
 }
