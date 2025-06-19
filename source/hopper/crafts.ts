53a21dc9497e781163b3cbab3251156e4a4cb04d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-19 02:49:16 -0400

    get this out of my face

--- source/hopper/crafts.ts
+++ source/hopper/crafts.ts
@@ -23,25 +23,5 @@ core.register_craft({
 	]
 })
 
-if not hopper.config.single_craftable_item then
-	core.register_craft({
-		output : "hopper:hopper_side",
-		recipe : {
-			{"main:iron","utility:chest","main:iron"},
-			{"","","main:iron"},
-		}
-	})
 
-	core.register_craft({
-		output : "hopper:hopper_side",
-		type:"shapeless",
-		recipe : {"hopper:hopper"},
-	})
-
-	core.register_craft({
-		output : "hopper:hopper",
-		type:"shapeless",
-		recipe : {"hopper:hopper_side"},
-	})
-end
 }
