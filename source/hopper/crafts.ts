128c2ef3e5372f3d62afb1469a0177d0659a21ff
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-19 02:47:36 -0400

    Update crafts.ts

--- source/hopper/crafts.ts
+++ source/hopper/crafts.ts
@@ -1,45 +1,47 @@
-// core.register_craft({
-// 	output = "hopper:hopper",
-// 	recipe = {
-// 		{"main:iron","utility:chest","main:iron"},
-// 		{"","main:iron",""},
-// 	}
-// })
+namespace hopper {
+core.register_craft({
+	output = "hopper:hopper",
+	recipe = {
+		{"main:iron","utility:chest","main:iron"},
+		{"","main:iron",""},
+	}
+})
 
-// core.register_craft({
-// 	output = "hopper:chute",
-// 	recipe = {
-// 		{"main:iron","utility:chest","main:iron"},
-// 	}
-// })
+core.register_craft({
+	output = "hopper:chute",
+	recipe = {
+		{"main:iron","utility:chest","main:iron"},
+	}
+})
 
-// core.register_craft({
-// 	output = "hopper:sorter",
-// 	recipe = {
-// 		{"","main:gold",""},
-// 		{"main:iron","utility:chest","main:iron"},
-// 		{"","main:iron",""},
-// 	}
-// })
+core.register_craft({
+	output = "hopper:sorter",
+	recipe = {
+		{"","main:gold",""},
+		{"main:iron","utility:chest","main:iron"},
+		{"","main:iron",""},
+	}
+})
 
-// if not hopper.config.single_craftable_item then
-// 	core.register_craft({
-// 		output = "hopper:hopper_side",
-// 		recipe = {
-// 			{"main:iron","utility:chest","main:iron"},
-// 			{"","","main:iron"},
-// 		}
-// 	})
+if not hopper.config.single_craftable_item then
+	core.register_craft({
+		output = "hopper:hopper_side",
+		recipe = {
+			{"main:iron","utility:chest","main:iron"},
+			{"","","main:iron"},
+		}
+	})
 
-// 	core.register_craft({
-// 		output = "hopper:hopper_side",
-// 		type="shapeless",
-// 		recipe = {"hopper:hopper"},
-// 	})
+	core.register_craft({
+		output = "hopper:hopper_side",
+		type="shapeless",
+		recipe = {"hopper:hopper"},
+	})
 
-// 	core.register_craft({
-// 		output = "hopper:hopper",
-// 		type="shapeless",
-// 		recipe = {"hopper:hopper_side"},
-// 	})
-// end
+	core.register_craft({
+		output = "hopper:hopper",
+		type="shapeless",
+		recipe = {"hopper:hopper_side"},
+	})
+end
+}
