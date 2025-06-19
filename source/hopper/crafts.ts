1b656124bd0209dd09c9c2c067c3d1859b382c28
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-19 02:49:21 -0400

    Update crafts.ts

--- source/hopper/crafts.ts
+++ source/hopper/crafts.ts
@@ -1,27 +1,23 @@
 namespace hopper {
-core.register_craft({
-	output : "hopper:hopper",
-	recipe : [
-		["main:iron","utility:chest","main:iron"],
-		["","main:iron",""],
-	]
-})
+	core.register_craft({
+		output: "hopper:hopper",
+		recipe: [
+			["main:iron", "utility:chest", "main:iron"],
+			["", "main:iron", ""],
+		],
+	});
 
-core.register_craft({
-	output : "hopper:chute",
-	recipe : [
-		["main:iron","utility:chest","main:iron"],
-	]
-})
+	core.register_craft({
+		output: "hopper:chute",
+		recipe: [["main:iron", "utility:chest", "main:iron"]],
+	});
 
-core.register_craft({
-	output : "hopper:sorter",
-	recipe : [
-		["","main:gold",""],
-		["main:iron","utility:chest","main:iron"],
-		["","main:iron",""],
-	]
-})
-
-
+	core.register_craft({
+		output: "hopper:sorter",
+		recipe: [
+			["", "main:gold", ""],
+			["main:iron", "utility:chest", "main:iron"],
+			["", "main:iron", ""],
+		],
+	});
 }
