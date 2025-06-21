a5bd95f303637e6fb76d102254d8a39cbe35deab
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-21 02:27:06 -0400

    Update abms.ts

--- source/hopper/abms.ts
+++ source/hopper/abms.ts
@@ -157,14 +157,14 @@ namespace hopper {
 		},
 	};
 
-	// const dirs: Vec3[] = [
-	// {[0]={x=0, y=-1, z=0},
-	// 		{x=0, y=0, z=-1},
-	// 		{x=0, y=0, z=1},
-	// 		{x=-1, y=0, z=0},
-	// 		{x=1, y=0, z=0},
-	// 		{x=0, y=1, z=0}}
-	// ]
+	const dirs: Dictionary<number, Vec3> = {
+		// {[0]={x=0, y=-1, z=0},
+		// 		{x=0, y=0, z=-1},
+		// 		{x=0, y=0, z=1},
+		// 		{x=-1, y=0, z=0},
+		// 		{x=1, y=0, z=0},
+		// 		{x=0, y=1, z=0}}
+	};
 
 	// local bottomdir = function(facedir)
 	// 	return ()[math.floor(facedir/4)]
