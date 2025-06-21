781cd380de621a619b95cb4549f865afe6cb1125
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-21 03:45:44 -0400

    Update hoppers.ts

--- source/hopper/nodes/hoppers.ts
+++ source/hopper/nodes/hoppers.ts
@@ -180,11 +180,11 @@ namespace hopper {
 		},
 	});
 
-	let hopper_side_drop;
-	let hopper_groups;
-
-	hopper_side_drop = "crafter_hopper:hopper";
-	hopper_groups = { cracky: 3, not_in_creative_inventory: 1 };
+	const hopper_side_drop: string = "crafter_hopper:hopper";
+	const hopper_groups: Dictionary<string, number> = {
+		cracky: 3,
+		not_in_creative_inventory: 1,
+	};
 
 	// core.register_node("crafter_hopper:hopper_side", {
 	// 	description = S("Side Hopper"),
