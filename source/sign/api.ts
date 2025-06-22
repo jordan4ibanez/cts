3f7ddd04541172ead902afaa254167fcfec8bc6f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-22 14:05:48 -0400

    Update api.ts

--- source/sign/api.ts
+++ source/sign/api.ts
@@ -579,37 +579,37 @@ namespace sign {
 				["^g"]: string.char(0x90),
 				["^h"]: string.char(0x91),
 			});
-			// 		local word_l = #word
-			// 		local i = 1
-			// 		while i <= word_l  do
-			// 			local c = word:sub(i, i)
-			// 			if c == "#" then
-			// 				local cc = tonumber(word:sub(i+1, i+1), 16)
-			// 				if cc then
-			// 					i = i + 1
-			// 					cur_color = cc
-			// 				end
-			// 			else
-			// 				local w = cwidth_tab[c]
-			// 				if w then
-			// 					width = width + w + 1
-			// 					if width >= (line_width - cwidth_tab[" "]) then
-			// 						width = 0
-			// 					else
-			// 						maxw = math_max(width, maxw)
-			// 					end
-			// 					if #chars < MAX_INPUT_CHARS then
-			// 						table.insert(chars, {
-			// 							off = ch_offs,
-			// 							tex = char_tex(font_name, c),
-			// 							col = ("%X"):format(cur_color),
-			// 						})
-			// 					end
-			// 					ch_offs = ch_offs + w
-			// 				end
-			// 			end
-			// 			i = i + 1
-			// 		end
+			let word_l = word.length;
+			let i = 1;
+			while (i <= word_l) {
+				const c: string = string.sub(word, i, i);
+				// 			if c == "#" then
+				// 				local cc = tonumber(word:sub(i+1, i+1), 16)
+				// 				if cc then
+				// 					i = i + 1
+				// 					cur_color = cc
+				// 				end
+				// 			else
+				// 				local w = cwidth_tab[c]
+				// 				if w then
+				// 					width = width + w + 1
+				// 					if width >= (line_width - cwidth_tab[" "]) then
+				// 						width = 0
+				// 					else
+				// 						maxw = math_max(width, maxw)
+				// 					end
+				// 					if #chars < MAX_INPUT_CHARS then
+				// 						table.insert(chars, {
+				// 							off = ch_offs,
+				// 							tex = char_tex(font_name, c),
+				// 							col = ("%X"):format(cur_color),
+				// 						})
+				// 					end
+				// 					ch_offs = ch_offs + w
+				// 				end
+				// 			end
+				i = i + 1;
+			}
 			// 		width = width + cwidth_tab[" "] + 1
 			// 		maxw = math_max(width, maxw)
 			// 		table.insert(words, { chars=chars, w=ch_offs })
