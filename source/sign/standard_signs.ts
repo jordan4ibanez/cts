c598f830983c090564da43d89c2bdeb706b8ddeb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-22 10:55:41 -0400

    Update standard_signs.ts

--- source/sign/standard_signs.ts
+++ source/sign/standard_signs.ts
@@ -1,33 +1,29 @@
 namespace sign {
-// -- Definitions for standard minetest_game wooden and steel wall signs
-
-// signs_lib.register_sign("sign:sign", {
-// 	description = "Sign",
-// 	inventory_image = "signs_lib_sign_wall_wooden_inv.png",
-// 	tiles = {
-// 		"signs_lib_sign_wall_wooden.png",
-// 		"signs_lib_sign_wall_wooden_edges.png",
-// 		-- items 3 - 5 are not set, so signs_lib will use its standard pole
-// 		-- mount, hanging, and yard sign stick textures.
-// 	},
-
-// 	allow_hanging = true,
-// 	allow_widefont = true,
-// 	allow_onpole = true,
-// 	allow_onpole_horizontal = true,
-// 	allow_yard = true
-// })
-
-// minetest.register_craft({
-// 	output = "sign:sign 3",
-// 	recipe = {
-// 		{"main:wood","main:wood","main:wood"},
-// 		{"main:wood","main:wood","main:wood"},
-// 		{"","main:stick",""}
-// 	}
-// })
-
-/*
+	// -- Definitions for standard minetest_game wooden and steel wall signs
+	// signs_lib.register_sign("sign:sign", {
+	// 	description = "Sign",
+	// 	inventory_image = "signs_lib_sign_wall_wooden_inv.png",
+	// 	tiles = {
+	// 		"signs_lib_sign_wall_wooden.png",
+	// 		"signs_lib_sign_wall_wooden_edges.png",
+	// 		-- items 3 - 5 are not set, so signs_lib will use its standard pole
+	// 		-- mount, hanging, and yard sign stick textures.
+	// 	},
+	// 	allow_hanging = true,
+	// 	allow_widefont = true,
+	// 	allow_onpole = true,
+	// 	allow_onpole_horizontal = true,
+	// 	allow_yard = true
+	// })
+	// minetest.register_craft({
+	// 	output = "sign:sign 3",
+	// 	recipe = {
+	// 		{"main:wood","main:wood","main:wood"},
+	// 		{"main:wood","main:wood","main:wood"},
+	// 		{"","main:stick",""}
+	// 	}
+	// })
+	/*
 // table.insert(signs_lib.lbm_restore_nodes, "signs:sign_hanging")
 // table.insert(signs_lib.lbm_restore_nodes, "basic_signs:hanging_sign")
 // table.insert(signs_lib.lbm_restore_nodes, "signs:sign_yard")
