78aabb76774e56739a398496d5201a745303164f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-22 10:56:18 -0400

    Update standard_signs.ts

--- source/sign/standard_signs.ts
+++ source/sign/standard_signs.ts
@@ -1,13 +1,13 @@
 namespace sign {
-	// -- Definitions for standard minetest_game wooden and steel wall signs
+	// // Definitions for standard minetest_game wooden and steel wall signs
 	// signs_lib.register_sign("sign:sign", {
 	// 	description = "Sign",
 	// 	inventory_image = "signs_lib_sign_wall_wooden_inv.png",
 	// 	tiles = {
 	// 		"signs_lib_sign_wall_wooden.png",
 	// 		"signs_lib_sign_wall_wooden_edges.png",
-	// 		-- items 3 - 5 are not set, so signs_lib will use its standard pole
-	// 		-- mount, hanging, and yard sign stick textures.
+	// 		// items 3 - 5 are not set, so signs_lib will use its standard pole
+	// 		// mount, hanging, and yard sign stick textures.
 	// 	},
 	// 	allow_hanging = true,
 	// 	allow_widefont = true,
@@ -15,7 +15,7 @@ namespace sign {
 	// 	allow_onpole_horizontal = true,
 	// 	allow_yard = true
 	// })
-	// minetest.register_craft({
+	// core.register_craft({
 	// 	output = "sign:sign 3",
 	// 	recipe = {
 	// 		{"main:wood","main:wood","main:wood"},
