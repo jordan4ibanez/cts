89ce2816249592b3d13f84d6adbcc87d74c02e10
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-24 04:20:51 -0400

    dead code

--- source/sign/api.ts
+++ source/sign/api.ts
@@ -861,27 +861,6 @@ namespace sign {
 			);
 			update_sign(pos, fields);
 		}
-		// else if (fields.on || fields.off) {
-		// 		local node = core.get_node(pos)
-		// 		local meta = core.get_meta(pos)
-		// 		local change
-		// 		if fields.on and meta:get_int("widefont") == 1 then
-		// 			meta:set_int("widefont", 0)
-		// 			change = true
-		// 		elseif fields.off and meta:get_int("widefont") == 0 then
-		// 			meta:set_int("widefont", 1)
-		// 			change = true
-		// 		end
-		// 		if change then
-		// 			core.log("action", S("@1 flipped the wide-font switch to \"@2\" at @3",
-		// 				(sender:get_player_name() or ""),
-		// 				(fields.on and "off" or "on"),
-		// 				core.pos_to_string(pos)
-		// 			))
-		// 			signs_lib.construct_sign(pos)
-		// 			signs_lib.update_sign(pos, fields)
-		// 		end
-		// }
 	}
 
 	// Make selection boxes.
