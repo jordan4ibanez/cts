588d5106b777800ebaffb7fc6554090a41ed4681
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-24 07:51:34 -0400

    I'll be taking that

--- source/sign/standard_signs.ts
+++ source/sign/standard_signs.ts
@@ -1,15 +1,5 @@
 namespace sign {
-	sign.register_sign("crafter_sign:sign", {
-		description: "Sign",
-		inventory_image: "signs_lib_sign_wall_wooden_inv.png",
-		wield_image: "signs_lib_sign_wall_wooden_inv.png",
-		tiles: [
-			"signs_lib_sign_wall_wooden.png",
-			"signs_lib_sign_wall_wooden_edges.png",
-			// items 3 - 5 are not set, so signs_lib will use its standard pole
-			// mount, hanging, and yard sign stick textures.
-		],
-	});
+	
 
 	core.register_craft({
 		output: "crafter_sign:sign_wall 3",
