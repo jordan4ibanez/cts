e2c49611b096f3a70d28e4825b817b71a7b3f031
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-25 17:50:29 -0400

    Update init.ts

--- source/weather/init.ts
+++ source/weather/init.ts
@@ -134,47 +134,43 @@ namespace weather {
 
 	//? This is debug.
 	//local average = {}
-	// function XOR( num1, num2 )
-	// 	// This XOR function is excerpted from the Bitwise Operations Mod v1.2, by Leslie E. Krause
-	// 	// which is provided under the MIT License (MIT)
-	// 	//
-	// 	// The MIT License (MIT)
-	// 	//
-	// 	// Copyright (c) 2020, Leslie Krause (leslie@searstower.org)
-	// 	//
-	// 	// Permission is hereby granted, free of charge, to any person obtaining a copy of this
-	// 	// software and associated documentation files (the "Software"), to deal in the Software
-	// 	// without restriction, including without limitation the rights to use, copy, modify, merge,
-	// 	// publish, distribute, sublicense, and/or sell copies of the Software, and to permit
-	// 	// persons to whom the Software is furnished to do so, subject to the following conditions:
-	// 	//
-	// 	// The above copyright notice and this permission notice shall be included in all copies or
-	// 	// substantial portions of the Software.
-	// 	//
-	// 	// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-	// 	// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-	// 	// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
-	// 	// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-	// 	// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-	// 	// DEALINGS IN THE SOFTWARE.
-	// 	//
-	// 	// For more details:
-	// 	// https://opensource.org/licenses/MIT
-	// 	local exp = 1
-	// 	local res = 0
-	// 	while num1 > 0 or num2 > 0 do
-	// 		local rem1 = num1 % 2
-	// 		local rem2 = num2 % 2
-	// 		if rem1 ~= rem2 then
-	// 			// set each bit
-	// 			res = res + exp
-	// 		end
-	// 		num1 = ( num1 - rem1 ) / 2
-	// 		num2 = ( num2 - rem2 ) / 2
-	// 		exp = exp * 2
-	// 	end
-	// 	return res
-	// end
+
+	function XOR(num1: number, num2: number): number {
+		// This XOR function is excerpted from the Bitwise Operations Mod v1.2, by Leslie E. Krause
+		// which is provided under the MIT License (MIT)
+		// The MIT License (MIT)
+		// Copyright (c) 2020, Leslie Krause (leslie@searstower.org)
+		// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+		// software and associated documentation files (the "Software"), to deal in the Software
+		// without restriction, including without limitation the rights to use, copy, modify, merge,
+		// publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+		// persons to whom the Software is furnished to do so, subject to the following conditions:
+		//
+		// The above copyright notice and this permission notice shall be included in all copies or
+		// substantial portions of the Software.
+		//
+		// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+		// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+		// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+		// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+		// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+		// DEALINGS IN THE SOFTWARE.
+		// 	local exp = 1
+		// 	local res = 0
+		// 	while num1 > 0 or num2 > 0 do
+		// 		local rem1 = num1 % 2
+		// 		local rem2 = num2 % 2
+		// 		if rem1 ~= rem2 then
+		// 			// set each bit
+		// 			res = res + exp
+		// 		end
+		// 		num1 = ( num1 - rem1 ) / 2
+		// 		num2 = ( num2 - rem2 ) / 2
+		// 		exp = exp * 2
+		// 	end
+		// 	return res
+	}
+
 	// local function do_snow()
 	// 	if weather_type == 1 then
 	// 		for _,player in ipairs(core.get_connected_players()) do
