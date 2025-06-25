4e409eef330928b802e061da04b974aa8b4c1d3f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-25 15:24:17 -0400

    Put that in there

--- source/hopper/crafts.ts
+++ source/hopper/crafts.ts
@@ -1,11 +1,5 @@
 namespace hopper {
-	// core.register_craft({
-	// 	output: "crafter_hopper:hopper",
-	// 	recipe: [
-	// 		["crafter:iron", "crafter_chest:chest", "crafter:iron"],
-	// 		["", "crafter:iron", ""],
-	// 	],
-	// });
+	
 	// core.register_craft({
 	// 	output: "crafter_hopper:chute",
 	// 	recipe: [["crafter:iron", "crafter_chest:chest", "crafter:iron"]],
--- source/hopper/hoppers.ts
+++ source/hopper/hoppers.ts
@@ -502,4 +502,12 @@ namespace hopper {
 			);
 		},
 	});
+
+	core.register_craft({
+		output: "crafter_hopper:hopper",
+		recipe: [
+			["crafter:iron", "crafter_chest:chest", "crafter:iron"],
+			["", "crafter:iron", ""],
+		],
+	});
 }
