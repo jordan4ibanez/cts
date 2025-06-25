48ce5a1dde20927703e9387607f8cc94a63aa78c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-25 17:53:46 -0400

    Update init.ts

--- source/weather/init.ts
+++ source/weather/init.ts
@@ -145,30 +145,30 @@ namespace weather {
 		// without restriction, including without limitation the rights to use, copy, modify, merge,
 		// publish, distribute, sublicense, and/or sell copies of the Software, and to permit
 		// persons to whom the Software is furnished to do so, subject to the following conditions:
-		//
 		// The above copyright notice and this permission notice shall be included in all copies or
 		// substantial portions of the Software.
-		//
 		// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
 		// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
 		// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
 		// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 		// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 		// DEALINGS IN THE SOFTWARE.
-		// 	local exp = 1
-		// 	local res = 0
-		// 	while num1 > 0 or num2 > 0 do
-		// 		local rem1 = num1 % 2
-		// 		local rem2 = num2 % 2
-		// 		if rem1 ~= rem2 then
-		// 			// set each bit
-		// 			res = res + exp
-		// 		end
-		// 		num1 = ( num1 - rem1 ) / 2
-		// 		num2 = ( num2 - rem2 ) / 2
-		// 		exp = exp * 2
-		// 	end
-		// 	return res
+		let exp: number = 1;
+		let res = 0;
+		while (num1 > 0 || num2 > 0) {
+			const rem1: number = num1 % 2;
+			const rem2: number = num2 % 2;
+
+			if (rem1 != rem2) {
+				// Set each bit.
+				res = res + exp;
+			}
+
+			num1 = (num1 - rem1) / 2;
+			num2 = (num2 - rem2) / 2;
+			exp = exp * 2;
+		}
+		return res;
 	}
 
 	// local function do_snow()
