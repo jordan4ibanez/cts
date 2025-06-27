284594ae723c6889bf701982a3298c398de64238
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-27 16:49:27 -0400

    Update commands.ts

--- source/weather/commands.ts
+++ source/weather/commands.ts
@@ -3,17 +3,17 @@ namespace weather {
 		params: "<mobname>",
 		description: "Spawn a mob",
 		privs: { server: true },
-		func: (name: string, weather: string) => {
-			if (weather == "0" || weather == "clear") {
+		func: (name: string, newWeather: string) => {
+			if (newWeather == "0" || newWeather == "clear") {
 				setWeatherType(0);
 				core.chat_send_all(name + " has set the weather to clear!");
-			} else if (weather == "1" || weather == "snow") {
+			} else if (newWeather == "1" || newWeather == "snow") {
 				setWeatherType(1);
 				core.chat_send_all(name + " has set the weather to snow!");
-			} else if (weather == "2" || weather == "rain") {
+			} else if (newWeather == "2" || newWeather == "rain") {
 				setWeatherType(2);
 				core.chat_send_all(name + " has set the weather to rain!");
-			} else if (weather == "") {
+			} else if (newWeather == "") {
 				core.chat_send_player(
 					name,
 					"Possible weather types are: 0,clear,1,snow,2,rain"
@@ -21,7 +21,7 @@ namespace weather {
 			} else {
 				core.chat_send_player(
 					name,
-					'"' + weather + '"' + " is not a registered weather type!"
+					'"' + newWeather + '"' + " is not a registered weather type!"
 				);
 			}
 		},
