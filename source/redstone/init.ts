eb190b8ed077a4d0d9b9a341108f10118d20b02b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-27 18:14:27 -0400

    Update init.ts

--- source/redstone/init.ts
+++ source/redstone/init.ts
@@ -98,22 +98,22 @@ namespace redstone {
 	// math.abs is not needed.
 
 	const order: Vec3[] = [
-		{x: 0,y: 0,z: 0},
-		{x: 1,y: 0,z: 0},
-		{x:-1,y: 0,z: 0},
-		{x: 0,y: 0,z: 1},
-		{x: 0,y: 0,z:-1},
-		{x: 0,y: 1,z: 0},
-		{x: 0,y:-1,z: 0},
-		{x: 1,y: 1,z: 0},
-		{x:-1,y: 1,z: 0},
-		{x: 0,y: 1,z: 1},
-		{x: 0,y: 1,z:-1},
-		{x: 1,y:-1,z: 0},
-		{x:-1,y:-1,z: 0},
-		{x: 0,y:-1,z: 1},
-		{x: 0,y:-1,z:-1},
-	]
+		vector.create3d({ x: 0, y: 0, z: 0 }),
+		vector.create3d({ x: 1, y: 0, z: 0 }),
+		vector.create3d({ x: -1, y: 0, z: 0 }),
+		vector.create3d({ x: 0, y: 0, z: 1 }),
+		vector.create3d({ x: 0, y: 0, z: -1 }),
+		vector.create3d({ x: 0, y: 1, z: 0 }),
+		vector.create3d({ x: 0, y: -1, z: 0 }),
+		vector.create3d({ x: 1, y: 1, z: 0 }),
+		vector.create3d({ x: -1, y: 1, z: 0 }),
+		vector.create3d({ x: 0, y: 1, z: 1 }),
+		vector.create3d({ x: 0, y: 1, z: -1 }),
+		vector.create3d({ x: 1, y: -1, z: 0 }),
+		vector.create3d({ x: -1, y: -1, z: 0 }),
+		vector.create3d({ x: 0, y: -1, z: 1 }),
+		vector.create3d({ x: 0, y: -1, z: -1 }),
+	];
 
 	// //thanks to RhodiumToad for helping me figure out a good method to do this
 	// local pool = {} // this holds all redstone data (literal 3d virtual memory map)
