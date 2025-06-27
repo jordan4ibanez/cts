e97eefe3dd24fb9d6305afb74c3fd83853ee8d2e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-27 18:42:42 -0400

    I'm sure this will have 0 side effects

--- source/redstone/init.ts
+++ source/redstone/init.ts
@@ -117,53 +117,13 @@ namespace redstone {
 
 	// Thanks to RhodiumToad for helping me figure out a good method to do this.
 	// This holds all redstone data (literal 3d virtual memory map).
-	const pool: Dictionary<
-		number,
-		Dictionary<number, Dictionary<number, any>>
-	> = {};
+	const pool = new Map<Vec3, any>();
+
 	function data_injection(pos: Vec3, data: any) {
-		// Add data into 3d memory.
 		if (data != null) {
-			const xDict: Dictionary<number, Dictionary<number, any>> = pool[
-				pos.x
-			] || (pool[pos.x] = {});
-
-			const yDict: Dictionary<number, any> =
-				xDict[pos.y] || (xDict[pos.y] = {});
-
-			yDict[pos.z] = data;
-			// 		//instructions = instructions + 1
-			// 		//print("building 3d memory")
-			// 	//delete data from 3d memory
+			pool.set(pos, data);
 		} else {
-			const xDict:
-				| Dictionary<number, Dictionary<number, any>>
-				| undefined = pool[pos.x];
-			if (xDict == null) {
-				return;
-			}
-
-			const yDict: Dictionary<number, any> | undefined = xDict[pos.y];
-
-			if (yDict == null) {
-				return;
-			}
-
-			
-			// 			pool[pos.x][pos.y][pos.z] = data
-			
-			// 			//instructions = instructions + 1
-			// 			//print("deleting 3d memory")
-			// 			if pool[pos.x][pos.y] and not next(pool[pos.x][pos.y]) then
-			// 				pool[pos.x][pos.y] = nil
-			// 				//instructions = instructions + 1
-			// 				// only run this if y axis is empty
-			// 				if pool[pos.x] and not next(pool[pos.x]) then
-			// 					pool[pos.x] = nil
-			// 					//instructions = instructions + 1
-			// 				end
-			// 			end
-			
+			pool.delete(pos);
 		}
 	}
 
