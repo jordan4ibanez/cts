0f7e94222ee48f072ab8ffb91c41ed6ead773075
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-27 16:34:39 -0400

    Update commands.ts

--- source/weather/commands.ts
+++ source/weather/commands.ts
@@ -11,14 +11,18 @@ namespace weather {
 				setWeatherType(1);
 				core.chat_send_all(name + " has set the weather to snow!");
 			} else if (weather == "2" || weather == "rain") {
-				// 			weather_type = 2
-				// 			function_send_weather_type()
-				// 			update_player_sky()
-				// 			minetest.chat_send_all(name+" has set the weather to rain!")
+				setWeatherType(2);
+				core.chat_send_all(name + " has set the weather to rain!");
 			} else if (weather == "") {
-				// 			minetest.chat_send_player(name, "Possible weather types are: 0,clear,1,snow,2,rain")
+				core.chat_send_player(
+					name,
+					"Possible weather types are: 0,clear,1,snow,2,rain"
+				);
 			} else {
-				// 			minetest.chat_send_player(name, '"'+weather+'"'+" is not a registered weather type!")
+				minetest.chat_send_player(
+					name,
+					'"' + weather + '"' + " is not a registered weather type!"
+				);
 			}
 		},
 	});
