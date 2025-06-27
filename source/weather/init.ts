fbaa5608489e67213da719f68e7d38bfaa4bcdd3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-27 15:58:35 -0400

    Update init.ts

--- source/weather/init.ts
+++ source/weather/init.ts
@@ -613,17 +613,17 @@ namespace weather {
 						(object.get_luaentity() as any).mob == true &&
 						object != this.owner)
 				) {
-					// 			object:punch(self.object, 2,
-					// 				{
-					// 				full_punch_interval=1.5,
-					// 				damage_groups = {damage=0,fleshy=0},
-					// 			})
-					// 			hit = true
-					// 			break
+					object.punch(this.object, 2, {
+						full_punch_interval: 1.5,
+						damage_groups: { damage: 0, fleshy: 0 },
+					});
+
+					hit = true;
+					break;
 				}
 			}
 
-			// 	if (self.oldvel and ((vel.x == 0 and self.oldvel.x ~= 0) or (vel.y == 0 and self.oldvel.y ~= 0) or (vel.z == 0 and self.oldvel.z ~= 0))) or hit == true then
+			// 	if (this.oldvel and ((vel.x == 0 and this.oldvel.x ~= 0) or (vel.y == 0 and this.oldvel.y ~= 0) or (vel.z == 0 and this.oldvel.z ~= 0))) or hit == true then
 			// 		//snowballs explode in the nether
 			// 		if pos.y <= -10033 and pos.y >= -20000 then
 			// 			this.object.remove()
@@ -658,7 +658,7 @@ namespace weather {
 			// 			this.object.remove()
 			// 		end
 			// 	end
-			// 	self.oldvel = vel
+			// 	this.oldvel = vel
 		}
 	}
 }
