ee869c2c636677eb07aff8ee5a82bf4f5deabb45
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-27 16:34:00 -0400

    Update commands.ts

--- source/weather/commands.ts
+++ source/weather/commands.ts
@@ -6,14 +6,10 @@ namespace weather {
 		func: (name: string, weather: string) => {
 			if (weather == "0" || weather == "clear") {
 				setWeatherType(0);
-				// 			function_send_weather_type()
-				// 			update_player_sky()
-				// 			minetest.chat_send_all(name+" has set the weather to clear!")
+				core.chat_send_all(name + " has set the weather to clear!");
 			} else if (weather == "1" || weather == "snow") {
-				// 			weather_type = 1
-				// 			function_send_weather_type()
-				// 			minetest.chat_send_all(name+" has set the weather to snow!")
-				// 			update_player_sky()
+				setWeatherType(1);
+				core.chat_send_all(name + " has set the weather to snow!");
 			} else if (weather == "2" || weather == "rain") {
 				// 			weather_type = 2
 				// 			function_send_weather_type()
