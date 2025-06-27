d6da08fc31ff627210b2b0dbbff8bf534582b2da
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-27 16:30:50 -0400

    Update commands.ts

--- source/weather/commands.ts
+++ source/weather/commands.ts
@@ -4,26 +4,26 @@ namespace weather {
 		description: "Spawn a mob",
 		privs: { server: true },
 		func: (name: string, weather: string) => {
-			// 		if weather == "0" or weather == "clear" then
-			// 			weather_type = 0
-			// 			function_send_weather_type()
-			// 			update_player_sky()
-			// 			minetest.chat_send_all(name.." has set the weather to clear!")
-			// 		elseif weather == "1" or weather == "snow" then
-			// 			weather_type = 1
-			// 			function_send_weather_type()
-			// 			minetest.chat_send_all(name.." has set the weather to snow!")
-			// 			update_player_sky()
-			// 		elseif  weather == "2" or weather == "rain" then
-			// 			weather_type = 2
-			// 			function_send_weather_type()
-			// 			update_player_sky()
-			// 			minetest.chat_send_all(name.." has set the weather to rain!")
-			// 		elseif weather == "" then
-			// 			minetest.chat_send_player(name, "Possible weather types are: 0,clear,1,snow,2,rain")
-			// 		else
-			// 			minetest.chat_send_player(name, '"'..weather..'"'.." is not a registered weather type!")
-			// 		end
+			if (weather == "0" || weather == "clear") {
+				// 			weather_type = 0
+				// 			function_send_weather_type()
+				// 			update_player_sky()
+				// 			minetest.chat_send_all(name.." has set the weather to clear!")
+			} else if (weather == "1" || weather == "snow") {
+				// 			weather_type = 1
+				// 			function_send_weather_type()
+				// 			minetest.chat_send_all(name.." has set the weather to snow!")
+				// 			update_player_sky()
+			} else if (weather == "2" || weather == "rain") {
+				// 			weather_type = 2
+				// 			function_send_weather_type()
+				// 			update_player_sky()
+				// 			minetest.chat_send_all(name.." has set the weather to rain!")
+			} else if (weather == "") {
+				// 			minetest.chat_send_player(name, "Possible weather types are: 0,clear,1,snow,2,rain")
+			} else {
+				// 			minetest.chat_send_player(name, '"'..weather..'"'.." is not a registered weather type!")
+			}
 		},
 	});
 }
