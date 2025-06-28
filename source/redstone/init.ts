9dd93882b9ae4956ba08fcc4c4a37fdb11e5cbcc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-28 10:21:26 -0400

    Some framework

--- source/redstone/init.ts
+++ source/redstone/init.ts
@@ -1,9 +1,8 @@
 namespace redstone {
 	// todo: Retool this entire mod as "bluestone" so I don't hear any complaints about mc
+	const hashPosition = core.hash_node_position;
 
 	export const maxState: number = 9;
-
-	// The entirety of redstone data is simulated in memory and reflected into the map.
 
 	// todo: Add a change queue. Reflect changes onto the map once every 0.2 seconds.
 
@@ -11,14 +10,26 @@ namespace redstone {
 		powerSource?: number;
 	}
 
+	// The entirety of redstone data is simulated in memory and reflected into the map in a designated interval (if any changes).
+	const memoryMap = new Map<number, RedstoneData>();
+
 	/**
-	 * 
+	 * Add data into the
 	 * @param pos The position where this data is to be added.
 	 * @param data The data of the position. This can be dust, or a power source, etc.
 	 */
-	export function addData(pos: Vec3, data: RedstoneData): void {}
+	export function addData(pos: Vec3, data: RedstoneData): void {
+		const index: number = hashPosition(pos);
+		memoryMap.set(index, data);
+	}
 
 	export function deleteData(pos: Vec3): void {}
+
+	
+
+	function enqueueUpdate(hashedPosition: number): void {
+
+	}
 
 	utility.loadFiles([
 		// "functions",
