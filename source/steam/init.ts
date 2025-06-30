09e513f957c085d58e9525c4a568d82115c641dc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-29 22:14:02 -0400

    This was surprisingly easy

--- source/steam/init.ts
+++ source/steam/init.ts
@@ -6,6 +6,12 @@ namespace steam {
 			visual: EntityVisual.mesh,
 			mesh: "steam_engine.gltf",
 			visual_size: { x: 1, y: 1 },
+			textures: [
+				"steam_engine_base.png",
+				"steam_engine.png",
+				"steam_engine_connecting_rod.png",
+				"steam_engine_connecting_rod.png",
+			],
 		};
 
 		on_activate(staticData: string, delta: number): void {
@@ -13,6 +19,18 @@ namespace steam {
 			pos.y += 0.5;
 			this.object.set_pos(pos);
 			this.object.set_animation({ x: 0, y: 1 }, 1, 0, true);
+		}
+
+		on_punch(
+			puncher: ObjectRef | null,
+			timeFromLastPunch: number | null,
+			toolCapabilities: ToolCapabilities | null,
+			dir: Vec3 | null,
+			damage: number
+		): void {
+			const newSpeed = math.random(0, 3) + math.random();
+			core.chat_send_all(tostring(newSpeed));
+			this.object.set_animation({ x: 0, y: 1 }, newSpeed, 0, true);
 		}
 	}
 
