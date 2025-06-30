f0e5cc3d0bf3010a24c095475bd1aca1be0e72d0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-30 18:26:40 -0400

    all of this

--- source/steam/init.ts
+++ source/steam/init.ts
@@ -48,12 +48,28 @@ namespace steam {
 		sounds: crafter.stoneSound(),
 	});
 
-	core.register_node("crafter_steam:firebox", {
-		drawtype: Drawtype.mesh,
-		mesh: "steam_firebox.gltf",
-		tiles: ["dirt.png"],
-		paramtype2: ParamType2["4dir"],
-		groups: { stone: 1, pathable: 1 },
-		sounds: crafter.stoneSound(),
-	});
+	const states = ["open", "closed"];
+
+	for (const index of $range(0, 1)) {
+		const currentState = states[index];
+		core.register_node("crafter_steam:firebox_" + currentState, {
+			drawtype: Drawtype.mesh,
+			use_texture_alpha: TextureAlpha.clip,
+			mesh: `steam_firebox_${currentState}.gltf`,
+			tiles: ["steam_firebox.png", "steam_firebox_doors.png"],
+			paramtype2: ParamType2["4dir"],
+			groups: { stone: 1, pathable: 1 },
+			sounds: crafter.stoneSound(),
+			on_rightclick(position, node, clicker, itemStack, pointedThing) {
+				const newIndex = (index + 1) % 2;
+
+				const newState = states[newIndex];
+
+				core.swap_node(position, {
+					name: "crafter_steam:firebox_" + newState,
+					param2: node.param2,
+				});
+			},
+		});
+	}
 }
