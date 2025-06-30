ac8b8d5ba8670a2d76b1e7b708a8dc14ca270c8f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-30 17:32:20 -0400

    Update init.ts

--- source/steam/init.ts
+++ source/steam/init.ts
@@ -42,6 +42,8 @@ namespace steam {
 	core.register_node("crafter_steam:boiler", {
 		drawtype: Drawtype.mesh,
 		mesh: "steam_boiler.gltf",
+		tiles: ["steam_boiler.png"],
 		groups: { stone: 1, pathable: 1 },
+		sounds: crafter.stoneSound(),
 	});
 }
