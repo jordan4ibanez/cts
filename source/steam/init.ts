05458b8390252bfef46f0aabb502f74b7b9a4d23
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-30 15:31:00 -0400

    Update init.ts

--- source/steam/init.ts
+++ source/steam/init.ts
@@ -8,32 +8,41 @@ namespace steam {
 			visual_size: { x: 1, y: 1 },
 			textures: [
 				"steam_engine_base.png",
-				"steam_engine.png",
-				"steam_engine_connecting_rod.png",
-				"steam_engine_connecting_rod.png",
+				"steam_engine_steam_inlet.png",
+				"stone.png",
+				"stone.png",
+				"stone.png",
+				"stone.png",
+				"stone.png",
+				"stone.png",
+
+				// "steam_engine_flywheel.png",
 			],
 		};
 
-		on_activate(staticData: string, delta: number): void {
-			const pos = vector.floor(this.object.get_pos());
-			pos.y += 0.5;
-			this.object.set_pos(pos);
-			this.object.set_animation({ x: 0, y: 1 }, 1, 0, true);
-		}
+		// on_activate(staticData: string, delta: number): void {
+		// 	const pos = vector.floor(this.object.get_pos());
+		// 	pos.y += 0.5;
+		// 	this.object.set_pos(pos);
+		// 	this.object.set_animation({ x: 0, y: 1 }, 1, 0, true);
+		// }
 
-		on_punch(
-			puncher: ObjectRef | null,
-			timeFromLastPunch: number | null,
-			toolCapabilities: ToolCapabilities | null,
-			dir: Vec3 | null,
-			damage: number
-		): void {
-			const newSpeed = math.random(0, 3) + math.random();
-			core.chat_send_all(tostring(newSpeed));
-
-			this.object.set_animation_frame_speed(newSpeed)
-			// this.object.set_animation({ x: 0, y: 1 }, newSpeed, 0, true);
+		on_step(delta: number, moveResult: MoveResult | null): void {
+			print(this.object.get_pos())
 		}
+		// on_punch(
+		// 	puncher: ObjectRef | null,
+		// 	timeFromLastPunch: number | null,
+		// 	toolCapabilities: ToolCapabilities | null,
+		// 	dir: Vec3 | null,
+		// 	damage: number
+		// ): void {
+		// 	const newSpeed = math.random(0, 3) + math.random();
+		// 	core.chat_send_all(tostring(newSpeed));
+
+		// 	this.object.set_animation_frame_speed(newSpeed)
+		// 	// this.object.set_animation({ x: 0, y: 1 }, newSpeed, 0, true);
+		// }
 	}
 
 	utility.registerTSEntity(SteamEngine);
