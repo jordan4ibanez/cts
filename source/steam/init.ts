5c991ee39993bc24ab0235a15c2ce46156cb02b6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-01 14:16:59 -0400

    Make my code easier to read

--- source/steam/init.ts
+++ source/steam/init.ts
@@ -112,30 +112,40 @@ namespace steam {
 			const yaw = core.dir_to_yaw(core.fourdir_to_dir(dir4));
 
 			//? The controller will always sit to the left, so, +90 degrees.
-			const controllerDir = core.yaw_to_dir(yaw + math.pi / 2);
+			const dirController = core.yaw_to_dir(yaw + math.pi / 2);
 
 			//? Grease point 1 will sit at the current position.
 			// This is just set like this to add clarity to what's going on.
 			const greasePosition1 = currentPosTarget;
 
 			//? Grease point 2 will sit to the right, so, -90 degrees.
-			const greasePoint2 = core.yaw_to_dir(yaw - math.pi / 2);
+			const grease2Dir = core.yaw_to_dir(yaw - math.pi / 2);
 
 			// So now, we have where things need to go.
 			// This should not be destroying random things that are not buildable to.
 			// It must check if it can replace them or else bail out.
 
-			print(vector.add(greasePosition1, controllerDir));
+			const controllerPosition = vector.add(
+				greasePosition1,
+				dirController
+			);
 
-			core.swap_node(vector.add(greasePosition1, controllerDir), {
+			const greasePosition2 = vector.add(greasePosition1, grease2Dir);
+
+			{
+			}
+
+			print(vector.add(greasePosition1, dirController));
+
+			core.swap_node(controllerPosition, {
 				name: "crafter:glass",
 			});
 
 			core.swap_node(greasePosition1, {
-				name: "crafter:stone",
+				name: "crafter:diamondblock",
 			});
 
-			core.swap_node(vector.add(greasePosition1, greasePoint2), {
+			core.swap_node(greasePosition2, {
 				name: "crafter:stone",
 			});
 		},
