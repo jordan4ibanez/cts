da1bc0b0579f5221fbd922cd529a639c938984d1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-06-30 20:55:17 -0400

    Update init.ts

--- source/steam/init.ts
+++ source/steam/init.ts
@@ -135,7 +135,21 @@ namespace steam {
 			// ],
 
 			// +X.
-			connect_right: [
+			// connect_right: [
+			// 	// Pipe.
+			// 	[-pD, -pD, -pD, pE, pD, pD],
+			// 	// Flange left Z axis.
+			// 	[pL, -pD, pD, fS, pD, fD],
+			// 	// Flange right Z axis.
+			// 	[pL, -pD, -fD, fS, pD, -pD],
+			// 	// Flange top.
+			// 	[pL, pD, -fD, fS, fD, fD],
+			// 	// Flange bottom.
+			// 	[pL, -pD, -fD, fS, -fD, fD],
+			// ],
+
+			// -X.
+			connect_left: [
 				// Pipe.
 				[-pD, -pD, -pD, pE, pD, pD],
 				// Flange left Z axis.
@@ -147,8 +161,6 @@ namespace steam {
 				// Flange bottom.
 				[pL, -pD, -fD, fS, -fD, fD],
 			],
-
-			// connect_left: [-0.5, -0.5, -0.5, 0.5, 0.5, 0.5],
 		},
 	});
 }
