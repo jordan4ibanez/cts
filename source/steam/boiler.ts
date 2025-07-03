a4c64c527c1a84124b69268f7c526ed57972502d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-02 21:14:13 -0400

    Boily boiling boiler things

--- source/steam/boiler.ts
+++ source/steam/boiler.ts
@@ -4,12 +4,22 @@ namespace steam {
 	// PSI.
 	const maxPressure = 300;
 
+	// The boiler explodes if it's empty at this pressure.
+	const emptyExplosionPressure = 100;
+
+	// Imperial.
+	const boilingTemp = 212;
+
 	class BoilerMeta extends utility.CrafterMeta {
 		temperature: number = 0;
 		/** Percentage. */
 		waterLevel: number = 0;
 		/** PSI. */
 		pressure: number = 0;
+	}
+
+	function boil(pos: Vec3): void {
+		const boilerData = utility.getMeta(pos, BoilerMeta);
 	}
 
 	core.register_node("crafter_steam:boiler", {
@@ -21,6 +31,7 @@ namespace steam {
 		sounds: crafter.stoneSound(),
 
 		on_timer(position, elapsed) {
+			boil(position);
 			timerStart(position);
 		},
 		on_construct(position) {
